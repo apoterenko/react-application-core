9ea47805db4466129c838a550882190c8c9fa6dd
apoterenko <a.poterenko@gmail.com>
2017-08-16 16:23:11 +0300

    *modify IApplicationState

--- src/ts/core/store/store.interface.ts
+++ src/ts/core/store/store.interface.ts
@@ -1,5 +1,7 @@
 import { IApplicationPermissionState } from '../permission/permission.interface';
+import { IApplicationRootState } from '../component/root/root.interface';
 
 export interface IApplicationState<TPermissionState extends IApplicationPermissionState> {
+  root: IApplicationRootState;
   permission: TPermissionState;
 }
