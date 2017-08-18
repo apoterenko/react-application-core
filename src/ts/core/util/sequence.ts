81cc0fa3ca47e34bdc200ad9b0000cdc418502ba
apoterenko <a.poterenko@gmail.com>
2017-08-18 14:09:04 +0300

    *version up to SequenceFunctionT

--- src/ts/core/util/sequence.ts
+++ src/ts/core/util/sequence.ts
@@ -1,4 +1,6 @@
-export function sequence(sourceFn: Function, nextFn: Function, scope?: any): Function {
+export type SequenceFunctionT = (...args) => any;
+
+export function sequence(sourceFn: SequenceFunctionT, nextFn: SequenceFunctionT, scope?: any): SequenceFunctionT {
   return function () {
     sourceFn.apply(scope || this, arguments);
     nextFn.apply(scope || this, arguments);
