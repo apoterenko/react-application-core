80f48b284e08f98a6f3268840d6010804a8683c1
apoterenko <a.poterenko@gmail.com>
2017-08-18 20:26:53 +0300

    *editableFormContainerFactory fixing generic

--- src/ts/core/component/form/form-container.factory.ts
+++ src/ts/core/component/form/form-container.factory.ts
@@ -1,11 +1,11 @@
-import { IBaseContainer, IBaseContainerInternalState } from '../base/base.interface';
+import { IBaseContainerInternalState } from '../base/base.interface';
 import { IApplicationState } from '../../store/store.interface';
 import { IApplicationPermissionState } from '../../permission/permission.interface';
 import { containerFactory } from '../store/container.factory';
-import { IFormContainerInternalProps, IFormEntity } from './form.interface';
+import { IFormContainer, IFormContainerInternalProps, IFormEntity } from './form.interface';
 import { ConnectorMapperT, IConnectorCtor } from '../store/container.interface';
 
-export const editableFormContainerFactory = <TContainer extends IBaseContainer<TInternalProps, TInternalState>,
+export const editableFormContainerFactory = <TContainer extends IFormContainer<TEntity, TInternalProps, TInternalState>,
                                              TEntity extends IFormEntity,
                                              TAppState extends IApplicationState<TPermissionState, TPermissions>,
                                              TInternalProps extends IFormContainerInternalProps<TEntity>,
