31efc725f031c680dc8cbf31698465a46b6bb564
apoterenko <a.poterenko@gmail.com>
2017-08-25 21:25:18 +0300

    *fixing export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 export * from './ts/core/component/index';
 export * from './ts/core/di/index';
-export * from './ts/core/i18n/index';
 export * from './ts/core/router/index';
 export * from './ts/core/store/index';
 export * from './ts/core/permission/index';
