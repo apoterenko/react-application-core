2b64ded6d4e575554e98424c9574d3763f53da52
apoterenko <a.poterenko@gmail.com>
2017-09-04 14:51:39 +0300

    *update utils

--- src/ts/core/util/fn.ts
+++ src/ts/core/util/fn.ts
@@ -1,6 +1,5 @@
 import { AnyT } from 'core/definition.interface';
 
-export const emptyFn = () => {};
 export type FunctionT = (...args) => AnyT;
 
 export function sequence(sourceFn: FunctionT, nextFn: FunctionT, nextFnScope?: AnyT): FunctionT {
@@ -14,12 +13,4 @@ export function and(sourceFn: FunctionT, nextFn: FunctionT, nextFnScope?: AnyT):
   return function() {
     return sourceFn.apply(this, arguments) && nextFn.apply(nextFnScope || this, arguments);
   };
-}
-
-export function replace(sourceScope: AnyT, sourceFn: FunctionT, targetFn: FunctionT): FunctionT {
-  const currentFn = sourceScope[sourceFn.name];
-  sourceScope[sourceFn.name] = emptyFn;
-  targetFn.call(sourceScope);
-  sourceScope[sourceFn.name] = currentFn;
-  return sourceFn;
 }
