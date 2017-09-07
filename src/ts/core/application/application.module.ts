919bdbd9f1e6db028c50d3d5f82c8788e751990f
apoterenko <a.poterenko@gmail.com>
2017-09-07 20:19:13 +0300

    *storage.service

--- src/ts/core/application/application.module.ts
+++ src/ts/core/application/application.module.ts
@@ -1,6 +1,8 @@
 import { appContainer, DI_TYPES } from 'core/di';
 import { EventManager } from 'core/event';
 import { DateConverter } from 'core/converter';
+import { StorageService } from 'core/storage';
 
 appContainer.bind(DI_TYPES.EventManager).to(EventManager);
 appContainer.bind(DI_TYPES.DateConverter).to(DateConverter);
+appContainer.bind(DI_TYPES.Storage).to(StorageService);
---
+++ src/ts/core/storage/storage.service.ts
@@ -0,0 +1,68 @@
+import * as store from 'store/dist/store.modern';
+import { injectable } from 'inversify';
+
+import { BASE_PATH, APP_VERSION } from 'core/env';
+import { IStorage } from 'core/storage';
+import { AnyT } from 'core/definition.interface';
+
+@injectable()
+export class StorageService implements IStorage {
+
+  private prefix = [APP_VERSION, location.port || '80', BASE_PATH.replace(/\//g, '')].join('#');
+
+  set enabled(value: boolean) {
+    this.storage.enabled = value;
+  }
+
+  set disabled(value: boolean) {
+    this.storage.disabled = value;
+  }
+
+  get enabled(): boolean {
+    return this.storage.enabled;
+  }
+
+  get disabled(): boolean {
+    return this.storage.disabled;
+  }
+
+  public set(key: string, value: AnyT): AnyT {
+    return this.storage.set(this.prefix + key, value);
+  }
+
+  public get(key: string): AnyT {
+    return this.storage.get(this.prefix + key);
+  }
+
+  public remove(key: string): void {
+    this.storage.remove(this.prefix + key);
+  }
+
+  public clear(): void {
+    this.storage.clear();
+  }
+
+  public transact(key: string, defaultValue: AnyT, transactionFn?: (val: AnyT) => void): void {
+    this.storage.transact(this.prefix + key, defaultValue, transactionFn);
+  }
+
+  public getAll(): AnyT {
+    return this.storage.getAll();
+  }
+
+  public serialize(value: AnyT): string {
+    return this.storage.serialize(value);
+  }
+
+  public deserialize(value: string): AnyT {
+    return this.storage.deserialize(value);
+  }
+
+  public forEach(command: (key: string, value: AnyT) => void): void {
+    this.storage.forEach((key: string, value: AnyT) => (command(this.prefix + key, value)));
+  }
+
+  private get storage(): IStorage {
+    return store;
+  }
+}
