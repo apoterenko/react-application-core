fdab93ab7f8b0e9d0e02925c3e099c262bd88fc2
apoterenko <a.poterenko@gmail.com>
2017-09-13 14:52:41 +0300

    *clone nodes & accumulate children

--- src/core/util/clone.ts
+++ src/core/util/clone.ts
@@ -1,26 +1,35 @@
 import * as React from 'react';
 
-import { AnyT, IKeyValue, ReactElementT } from 'core/definition.interface';
-import { isPrimitive } from 'core/util';
+import { isPrimitive, uuid } from 'core/util';
+import { AnyT, ReactElementT } from 'core/definition.interface';
 
 export function clone<TObject>(o: TObject): TObject {
   return JSON.parse(JSON.stringify(o));
 }
 
-export function cloneNodes(component: ReactElementT | React.PureComponent,
-                           mergedProps: IKeyValue,
-                           predicate: (child: ReactElementT) => boolean): AnyT[]  {
+export function cloneNodes<TProps>(component: ReactElementT | React.PureComponent,
+                                   mergedProps: TProps,
+                                   predicate: (child: ReactElementT) => boolean,
+                                   childrenMap?: Map<ReactElementT, string>): AnyT[]  {
   return React.Children.map(component.props.children, (child: React.ReactChild) => {
         if (isPrimitive(child)) {
           return child;
         } else {
           const reactChild = child as ReactElementT;
-          return React.cloneElement<{}, {}>(reactChild, {
-            ...(predicate(reactChild) ? mergedProps : {}),
-            ...{
-              children: cloneNodes(reactChild, mergedProps, predicate),
-            },
-          });
+          const uuidRef = uuid();
+          const isApplicable = predicate(reactChild);
+
+          const clonedChild = React.cloneElement<{ children: React.ReactChild[] }, {}>(
+              reactChild,
+              {
+                ...(isApplicable ? { ref: uuidRef, ...(mergedProps || {}) } : {}),
+                children: cloneNodes<TProps>(reactChild, mergedProps, predicate, childrenMap),
+              }
+          );
+          if (childrenMap && isApplicable) {
+            childrenMap.set(clonedChild, uuidRef);
+          }
+          return clonedChild;
         }
       },
   );
