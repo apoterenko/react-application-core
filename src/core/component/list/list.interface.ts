aba1175a193706430fd988d059bebe8c413c4f99
apoterenko <a.poterenko@gmail.com>
2017-09-13 18:49:50 +0300

    *list action types

--- src/core/component/list/list.interface.ts
+++ src/core/component/list/list.interface.ts
@@ -13,3 +13,11 @@ export const INITIAL_APPLICATION_LIST_STATE: IApplicationListState = {
   data: null,
   selected: null,
 };
+
+export const LIST_LOAD_ACTION_TYPE = 'list.load';
+export const LIST_LOAD_DONE_ACTION_TYPE = 'list.load.done';
+export const LIST_LOCK_ACTION_TYPE = 'list.lock';
+export const LIST_DESTROY_ACTION_TYPE = 'list.destroy';
+export const LIST_SELECT_ACTION_TYPE = 'list.select';
+export const LIST_DESELECT_ACTION_TYPE = 'list.deselect';
+export const LIST_UPDATE_ACTION_TYPE = 'list.update';
