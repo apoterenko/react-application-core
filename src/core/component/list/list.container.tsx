c3f2eac48ae576531d852ce626e7e9cf006ef9a3
apoterenko <a.poterenko@gmail.com>
2017-09-14 19:47:21 +0300

    *list.container

---
+++ src/core/component/list/list.container.tsx
@@ -0,0 +1,58 @@
+import * as React from 'react';
+
+import { BaseContainer } from 'core/component/base';
+import { IEntity } from 'core/definition.interface';
+
+import {
+  LIST_DESTROY_ACTION_TYPE,
+  LIST_LOAD_ACTION_TYPE,
+  LIST_SELECT_ACTION_TYPE,
+  IListContainer,
+  IListContainerInternalProps,
+} from './list.interface';
+import { List } from './list.component';
+
+export class ListContainer extends BaseContainer<IListContainerInternalProps, {}>
+    implements IListContainer {
+
+  constructor(props: IListContainerInternalProps) {
+    super(props);
+    this.onSelect = this.onSelect.bind(this);
+  }
+
+  public componentWillUnmount(): void {
+    this.dispatch(LIST_DESTROY_ACTION_TYPE);
+  }
+
+  public render(): JSX.Element {
+    const props = this.props;
+    const { list } = props;
+    return list.progress
+        ? (
+            <div className='mdc-layout-grid'>
+              <div className='mdc-layout-grid__inner'>
+                {this.t(props.progressMessage || 'Loading...')}
+              </div>
+            </div>
+        )
+        : (
+            <List items={this.listData || []}
+                  renderer={props.renderer}
+                  activeItem={list.selected}
+                  onClick={this.onSelect}>
+            </List>
+        );
+  }
+
+  public onSearch(value: string): void {
+    this.dispatch(LIST_LOAD_ACTION_TYPE, { query: value });
+  }
+
+  private onSelect(entity: IEntity): void {
+    this.dispatch(LIST_SELECT_ACTION_TYPE, { selected: entity });
+  }
+
+  private get listData(): IEntity[] {
+    return this.props.list.data;
+  }
+}
