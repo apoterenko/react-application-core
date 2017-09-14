1e93c46335d86a94ba24fe50d96a66506c223575
apoterenko <a.poterenko@gmail.com>
2017-09-14 20:39:54 +0300

    *defaultReducers

--- src/core/store/store.interface.ts
+++ src/core/store/store.interface.ts
@@ -1,8 +1,8 @@
 import { AnyT } from 'core/definition.interface';
-import { IApplicationRootState, INITIAL_APPLICATION_ROOT_STATE } from 'core/component/root';
-import { IApplicationPermissionState, INITIAL_PERMISSION_STATE } from 'core/permission';
-import { IApplicationUserState, INITIAL_USER_STATE } from 'core/user';
-import { IApplicationLayoutState, INITIAL_APPLICATION_LAYOUT_STATE } from 'core/component/layout';
+import { IApplicationRootState, INITIAL_APPLICATION_ROOT_STATE, rootReducer } from 'core/component/root';
+import { IApplicationPermissionState, INITIAL_PERMISSION_STATE, permissionReducer } from 'core/permission';
+import { IApplicationUserState, INITIAL_USER_STATE, userReducer } from 'core/user';
+import { IApplicationLayoutState, INITIAL_APPLICATION_LAYOUT_STATE, layoutReducer } from 'core/component/layout';
 
 export interface IApplicationState<TPermissionState extends IApplicationPermissionState<TPermissions>,
                                    TPermissions> {
@@ -17,4 +17,11 @@ export const INITIAL_APPLICATION_STATE: IApplicationState<IApplicationPermission
   root: INITIAL_APPLICATION_ROOT_STATE,
   user: INITIAL_USER_STATE,
   layout: INITIAL_APPLICATION_LAYOUT_STATE,
+};
+
+export const defaultReducers = {
+  permission: permissionReducer,
+  root: rootReducer,
+  user: userReducer,
+  layout: layoutReducer,
 };
