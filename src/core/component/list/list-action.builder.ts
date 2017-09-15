c4fbb787de07f752e03cb5bda128cd6754f4ed06
apoterenko <a.poterenko@gmail.com>
2017-09-15 17:35:57 +0300

    *list-action.builder: buildLoadErrorActionType

--- src/core/component/list/list-action.builder.ts
+++ src/core/component/list/list-action.builder.ts
@@ -4,11 +4,16 @@ import {
   LIST_LOAD_ACTION_TYPE,
   LIST_LOCK_ACTION_TYPE,
   LIST_SELECT_ACTION_TYPE,
+  LIST_LOAD_ERROR_ACTION_TYPE,
 } from './list.interface';
 
 export class ListActionBuilder {
   public static buildLoadActionType(section: string): string {
     return `${section}.${LIST_LOAD_ACTION_TYPE}`;
+  }
+
+  public static buildLoadErrorActionType(section: string): string {
+    return `${section}.${LIST_LOAD_ERROR_ACTION_TYPE}`;
   }
 
   public static buildSelectActionType(section: string): string {
