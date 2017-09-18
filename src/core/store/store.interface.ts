70a6288d77354850ef39f8ccfa97779b56ee3ed4
apoterenko <a.poterenko@gmail.com>
2017-09-18 17:26:48 +0300

    *introduce dictionaries.reducer & dictionaries.state

--- src/core/store/store.interface.ts
+++ src/core/store/store.interface.ts
@@ -1,8 +1,8 @@
 import { AnyAction } from 'redux';
 
-import { AnyT, IKeyValue } from 'core/definition.interface';
+import { AnyT } from 'core/definition.interface';
 import { IApplicationRootState, INITIAL_APPLICATION_ROOT_STATE, rootReducer } from 'core/component/root';
-import { IApplicationPermissionState, INITIAL_PERMISSION_STATE, permissionReducer } from 'core/permission';
+import { IApplicationPermissionsState, INITIAL_PERMISSION_STATE, permissionReducer } from 'core/permission';
 import { IApplicationUserState, INITIAL_USER_STATE, userReducer } from 'core/user';
 import { IApplicationLayoutState, INITIAL_APPLICATION_LAYOUT_STATE, layoutReducer } from 'core/component/layout';
 import {
@@ -10,22 +10,28 @@ import {
   notificationReducer,
   INITIAL_APPLICATION_NOTIFICATION_STATE,
 } from 'core/notification';
+import { dictionariesReducer, IApplicationDictionariesState } from 'core/dictionary';
 
-export interface IApplicationState<TPermissionState extends IApplicationPermissionState<TPermissions>,
+export interface IApplicationState<TDictionariesState extends IApplicationDictionariesState,
+                                   TPermissionState extends IApplicationPermissionsState<TPermissions>,
                                    TPermissions> {
   root: IApplicationRootState;
   permission: TPermissionState;
   user: IApplicationUserState;
   layout: IApplicationLayoutState;
   notification: IApplicationNotificationState;
+  dictionaries: TDictionariesState;
 }
 
-export const INITIAL_APPLICATION_STATE: IApplicationState<IApplicationPermissionState<AnyT>, AnyT> = {
+export const INITIAL_APPLICATION_STATE: IApplicationState<AnyT,
+                                                          IApplicationPermissionsState<AnyT>,
+                                                          AnyT> = {
   permission: INITIAL_PERMISSION_STATE,
   root: INITIAL_APPLICATION_ROOT_STATE,
   user: INITIAL_USER_STATE,
   layout: INITIAL_APPLICATION_LAYOUT_STATE,
   notification: INITIAL_APPLICATION_NOTIFICATION_STATE,
+  dictionaries: {},
 };
 
 export const defaultReducers = {
@@ -34,4 +40,5 @@ export const defaultReducers = {
   user: userReducer,
   layout: layoutReducer,
   notification: notificationReducer,
+  dictionaries: dictionariesReducer,
 };
