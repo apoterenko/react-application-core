2f150ea1f69fa10a740f04b4e82bff570b3ca526
apoterenko <a.poterenko@gmail.com>
2017-09-18 18:05:07 +0300

    *simplifying storeFactory generics

--- src/core/store/store.module.ts
+++ src/core/store/store.module.ts
@@ -7,14 +7,12 @@ import { PROD_MODE } from 'core/env';
 import { IApplicationPermissionsState } from 'core/permission';
 import { appContainer, DI_TYPES } from 'core/di';
 import { APPLICATION_STATE_KEY, IStorage } from 'core/storage';
-import { IApplicationDictionariesState } from 'core/dictionary';
 
 import { IApplicationState, INITIAL_APPLICATION_STATE } from './store.interface';
 
-export function storeFactory<TAppState extends IApplicationState<TDictionariesState, TPermissionsState, TPermissions>,
-                             TDictionariesState extends IApplicationDictionariesState,
-                             TPermissionsState extends IApplicationPermissionsState<TPermissions>,
-                             TPermissions>(applicationSettings?: IApplicationSettings, appMiddlewares?: Middleware[]): Store<TAppState> {
+export type ApplicationStateT = IApplicationState<{}, IApplicationPermissionsState<{}>, {}>;
+
+export function storeFactory(applicationSettings?: IApplicationSettings, appMiddlewares?: Middleware[]): Store<ApplicationStateT> {
   const middlewares = [effectsMiddleware].concat(appMiddlewares || []);
 
   const preloadedState = applicationSettings && applicationSettings.usePersistence
@@ -23,14 +21,14 @@ export function storeFactory<TAppState extends IApplicationState<TDictionariesSt
 
   const store = createStore(
       (state) => state,
-      (preloadedState || INITIAL_APPLICATION_STATE) as TAppState,
+      (preloadedState || INITIAL_APPLICATION_STATE) as ApplicationStateT,
       PROD_MODE
           ? applyMiddleware(...middlewares)
           : composeWithDevTools(applyMiddleware(...middlewares)),
   );
 
   // Configuring of store at runtime
-  appContainer.bind<Store<TAppState>>(DI_TYPES.Store).toConstantValue(store);
+  appContainer.bind<Store<ApplicationStateT>>(DI_TYPES.Store).toConstantValue(store);
   EffectsService.configure(appContainer, store);
 
   return store;
