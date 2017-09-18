db9756d00897b1b8a0855fa95dc0e16a814dd988
apoterenko <a.poterenko@gmail.com>
2017-09-18 17:05:23 +0300

    *storeFactory: TDictionariesState generic

--- src/core/store/store.module.ts
+++ src/core/store/store.module.ts
@@ -4,14 +4,16 @@ import { EffectsService, effectsMiddleware } from 'redux-effects-promise';
 
 import { IApplicationSettings } from 'core/settings';
 import { PROD_MODE } from 'core/env';
-import { IApplicationPermissionState } from 'core/permission';
+import { IApplicationPermissionsState } from 'core/permission';
 import { appContainer, DI_TYPES } from 'core/di';
 import { APPLICATION_STATE_KEY, IStorage } from 'core/storage';
+import { IApplicationDictionariesState } from 'core/dictionary';
 
 import { IApplicationState, INITIAL_APPLICATION_STATE } from './store.interface';
 
-export function storeFactory<TAppState extends IApplicationState<TPermissionState, TPermissions>,
-                             TPermissionState extends IApplicationPermissionState<TPermissions>,
+export function storeFactory<TAppState extends IApplicationState<TDictionariesState, TPermissionsState, TPermissions>,
+                             TDictionariesState extends IApplicationDictionariesState,
+                             TPermissionsState extends IApplicationPermissionsState<TPermissions>,
                              TPermissions>(applicationSettings?: IApplicationSettings, appMiddlewares?: Middleware[]): Store<TAppState> {
   const middlewares = [effectsMiddleware].concat(appMiddlewares || []);
 
