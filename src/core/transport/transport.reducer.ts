8deeaf3a384cc8b8244ecaa29a0dc2f12d624ce6
apoterenko <a.poterenko@gmail.com>
2017-10-05 21:48:04 +0300

    *transport.reducer

---
+++ src/core/transport/transport.reducer.ts
@@ -0,0 +1,44 @@
+import { AnyAction } from 'redux';
+
+import {
+  INITIAL_APPLICATION_TRANSPORT_STATE,
+  IApplicationTransportState,
+  TRANSPORT_REQUEST_ACTION_TYPE,
+  TRANSPORT_REQUEST_DONE_ACTION_TYPE,
+  TRANSPORT_REQUEST_ERROR_ACTION_TYPE,
+  TRANSPORT_DESTROY_ACTION_TYPE,
+  TRANSPORT_TOKEN_UPDATE_ACTION_TYPE,
+} from './transport.interface';
+
+export function transportReducer(state: IApplicationTransportState = INITIAL_APPLICATION_TRANSPORT_STATE,
+                                 action: AnyAction): IApplicationTransportState {
+  const operationId = action.data && action.data.operationId;
+  switch (action.type) {
+    case TRANSPORT_REQUEST_ACTION_TYPE:
+      return {
+        ...state,
+        queue: operationId
+            ? state.queue.concat(action.data.operationId)
+            : state.queue,
+      };
+    case TRANSPORT_REQUEST_ERROR_ACTION_TYPE:
+    case TRANSPORT_REQUEST_DONE_ACTION_TYPE:
+      return {
+        ...state,
+        queue: operationId
+            ? state.queue.filter((item) => item !== action.data.operationId)
+            : state.queue,
+      };
+    case TRANSPORT_DESTROY_ACTION_TYPE: {
+      return {
+          ...INITIAL_APPLICATION_TRANSPORT_STATE,
+      };
+    }
+    case TRANSPORT_TOKEN_UPDATE_ACTION_TYPE:
+      return {
+        ...state,
+        token: action.data.token,
+      };
+  }
+  return state;
+}
