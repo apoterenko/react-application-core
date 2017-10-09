e4324a5a5bafcbccc0b87dbb810b505c50591da1
apoterenko <a.poterenko@gmail.com>
2017-10-09 20:44:55 +0300

    *call uuid lib

--- src/core/util/uuid.ts
+++ src/core/util/uuid.ts
@@ -1,8 +1,5 @@
+import * as uuid0 from 'uuid';
+
 export function uuid() {
-  let dateNow = Date.now();
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-    const result = (dateNow + Math.random() * 16) % 16 | 0;
-    dateNow = Math.floor(dateNow / 16);
-    return (c === 'x' ? result : (result & 0x3 | 0x8)).toString(16);
-  });
+  return uuid0();
 }
