69bafe43dc4d7899fcbf67785626be18737bfc23
apoterenko <a.poterenko@gmail.com>
2017-10-10 19:00:59 +0300

    *converter.interface

--- src/core/converter/converter.interface.ts
+++ src/core/converter/converter.interface.ts
@@ -2,6 +2,7 @@ import { AnyT } from '../definition.interface';
 
 export interface IDateConverter {
   localeSpecificConstants: IDateLocaleSpecificConstants;
+  formatDateTime(date: string | Date, format?: string, parseFormat?: string): string;
   formatDate(date: AnyT, format?: string): string;
   tryConvertToDate(date: AnyT, format?: string): AnyT;
   getDateRangeFromDate(date: Date): Date[];
