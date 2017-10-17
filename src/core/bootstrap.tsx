caa68d21e5a7ea3587f009391f24e45bc497bce3
apoterenko <a.poterenko@gmail.com>
2017-10-17 21:30:34 +0300

    *bootstrap: connect props to the application container (ready props)

--- src/core/bootstrap.tsx
+++ src/core/bootstrap.tsx
@@ -1,15 +1,14 @@
 import * as React from 'react';
 import { render } from 'react-dom';
+import { connect, Provider } from 'react-redux';
 
-import { ApplicationContainer } from './component/application/application.container';
-import { IApplicationContainerProps } from './component/application/application.interface';
-import { ApplicationStateT, IApplicationState } from './store/store.interface';
-import { IApplicationDictionariesState } from './dictionary/dictionaries.interface';
-import { IApplicationPermissionsState } from './permission/permission.interface';
-import { AnyT } from './definition.interface';
-import { BASE_PATH } from './env/env.interface';
+import { appContainer, DI_TYPES } from './di';
+import { ApplicationContainer, IApplicationContainerProps } from './component/application';
+import { ApplicationStateT, IApplicationState } from './store';
+import { IApplicationDictionariesState } from './dictionary';
+import { IApplicationPermissionsState } from './permission';
 
-export interface IContainerBootstrapCtor<TContainer extends ApplicationContainer<TAppState, IApplicationContainerProps, TDictionariesState, TPermissionsState, TPermissions, TPermissionObject>,
+export interface IContainerBootstrapCtor<TContainer extends ApplicationContainer<TAppState, TDictionariesState, TPermissionsState, TPermissions, TPermissionObject>,
                                          TAppState extends IApplicationState<TDictionariesState, TPermissionsState, TPermissions>,
                                          TDictionariesState extends IApplicationDictionariesState,
                                          TPermissionsState extends IApplicationPermissionsState<TPermissions>,
@@ -19,19 +18,22 @@ export interface IContainerBootstrapCtor<TContainer extends ApplicationContainer
 }
 
 export function bootstrap(
-    applicationContainer: IContainerBootstrapCtor<ApplicationContainer<ApplicationStateT, IApplicationContainerProps, IApplicationDictionariesState, IApplicationPermissionsState<{}>, {}, {}>,
+    applicationContainer: IContainerBootstrapCtor<ApplicationContainer<ApplicationStateT, IApplicationDictionariesState, IApplicationPermissionsState<{}>, {}, {}>,
                                                   ApplicationStateT,
                                                   IApplicationDictionariesState,
                                                   IApplicationPermissionsState<{}>,
                                                   {},
                                                   {}>,
+    props?: IApplicationContainerProps,
     rootId = 'root',
-    basePath = BASE_PATH) {
-  const Component = applicationContainer as AnyT;
+    ) {
   const ready = () => {
+    const Component = connect((state: ApplicationStateT) => ({ ...state.applicationReady }), {})
+                                  (applicationContainer);
     render(
-        <Component basename={basePath}>
-        </Component>,
+        <Provider store={appContainer.get(DI_TYPES.Store)}>
+          <Component {...props}/>
+        </Provider>,
         document.getElementById(rootId),
     );
   };
