4bd4a624eb844e0d66d2629d33d04f7dbedf267e
apoterenko <a.poterenko@gmail.com>
2017-10-24 16:24:11 +0300

    *base.transport + doSaveEntity

--- src/core/transport/base.transport.ts
+++ src/core/transport/base.transport.ts
@@ -1,8 +1,23 @@
 import { provideInSingleton, lazyInject, DI_TYPES } from '../di';
 import { IApplicationTransport } from './transport.interface';
+import { IEntity } from '../definition.interface';
+import { IApiEntityRequest } from '../api';
 
 @provideInSingleton(BaseTransport)
 export class BaseTransport {
-
   @lazyInject(DI_TYPES.Transport) protected transport: IApplicationTransport;
+
+  protected doSaveEntity<TEntity extends IEntity>(
+      entityRequest: IApiEntityRequest<TEntity>): Promise<TEntity> {
+    const apiEntity = entityRequest.apiEntity;
+    return this.transport.request<TEntity>({
+      params: {
+        ...apiEntity.changes || {},
+        ...entityRequest.extraParams,
+        id: apiEntity.isNew ? null : apiEntity.id,
+      },
+      name: apiEntity.isNew ? entityRequest.addApi : entityRequest.editApi,
+      operation: apiEntity.operation,
+    });
+  }
 }
