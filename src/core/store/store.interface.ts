8a9ee8af500b8d8d76485bc789ea8dc5f0988f3d
apoterenko <a.poterenko@gmail.com>
2017-10-31 20:45:55 +0300

    *remove legacy INITIAL_APPLICATION_STATE

--- src/core/store/store.interface.ts
+++ src/core/store/store.interface.ts
@@ -1,25 +1,22 @@
 import { AnyAction } from 'redux';
 
 import { AnyT, IKeyValue } from '../definition.interface';
-import { IApplicationRootState, INITIAL_APPLICATION_ROOT_STATE, rootReducer } from '../component/root';
-import { IApplicationPermissionsState, INITIAL_PERMISSION_STATE, permissionReducer } from '../permission';
+import { IApplicationRootState, rootReducer } from '../component/root';
+import { IApplicationPermissionsState, permissionReducer } from '../permission';
 import { IApplicationUserState, userReducer } from '../user';
-import { IApplicationLayoutState, INITIAL_APPLICATION_LAYOUT_STATE, layoutReducer } from '../component/layout';
+import { IApplicationLayoutState, layoutReducer } from '../component/layout';
 import {
   IApplicationTransportState,
-  INITIAL_APPLICATION_TRANSPORT_STATE,
   transportReducer,
 } from '../transport';
 import {
   IApplicationNotificationState,
   notificationReducer,
-  INITIAL_APPLICATION_NOTIFICATION_STATE,
 } from '../notification';
 import { dictionariesReducer, IApplicationDictionariesState } from '../dictionary';
 import {
   applicationReadyReducer,
   IApplicationReadyState,
-  INITIAL_APPLICATION_READY_STATE,
 } from '../component/application';
 
 export interface IApplicationState<TDictionariesState extends IApplicationDictionariesState,
@@ -38,19 +35,6 @@ export interface IApplicationState<TDictionariesState extends IApplicationDictio
 export type ApplicationStateT = IApplicationState<IApplicationDictionariesState,
                                                   IApplicationPermissionsState<{}>,
                                                   {}>;
-
-export const INITIAL_APPLICATION_STATE: IApplicationState<IApplicationDictionariesState,
-                                                          IApplicationPermissionsState<AnyT>,
-                                                          AnyT> = {
-  applicationReady: INITIAL_APPLICATION_READY_STATE,
-  permission: INITIAL_PERMISSION_STATE,
-  root: INITIAL_APPLICATION_ROOT_STATE,
-  layout: INITIAL_APPLICATION_LAYOUT_STATE,
-  notification: INITIAL_APPLICATION_NOTIFICATION_STATE,
-  transport: INITIAL_APPLICATION_TRANSPORT_STATE,
-  dictionaries: {},
-  user: {},
-};
 
 export const defaultReducers = {
   applicationReady: applicationReadyReducer,
