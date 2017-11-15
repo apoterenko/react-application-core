aaffea202e7f3825a40a9dd0107cbe2deb756b10
apoterenko <a.poterenko@gmail.com>
2017-11-15 20:11:39 +0300

    *makeListLoadErrorEffectsProxy

--- src/core/store/effects/index.ts
+++ src/core/store/effects/index.ts
@@ -1,5 +1,6 @@
 export * from './paginated-list-effects.proxy';
 export * from './filtered-list-effects.proxy';
 export * from './application-prepare-error-effects.proxy';
+export * from './list-load-error-effects.proxy';
 export * from './effects-by.decorator';
 export * from './base.effects';
---
+++ src/core/store/effects/list-load-error-effects.proxy.ts
@@ -0,0 +1,19 @@
+import { EffectsService, IEffectsAction } from 'redux-effects-promise';
+
+import { ListActionBuilder } from '../../component/list';
+import { NotificationActionBuilder } from '../../notification';
+import { provideInSingleton } from '../../di';
+
+export function makeListLoadErrorEffectsProxy(section: string): () => void {
+  return (): void => {
+
+    @provideInSingleton(Effects)
+    class Effects {
+
+      @EffectsService.effects(ListActionBuilder.buildLoadErrorActionType(section))
+      public $onListLoadError(action: IEffectsAction): IEffectsAction {
+        return NotificationActionBuilder.buildErrorAction(action.error);
+      }
+    }
+  };
+}
