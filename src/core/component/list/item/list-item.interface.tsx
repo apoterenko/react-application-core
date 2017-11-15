8fdf7b88df1d9d884ef164c2eb77568e393b02a9
apoterenko <a.poterenko@gmail.com>
2017-11-15 21:52:25 +0300

    *list-item.interface: itemValue result typings fix

--- src/core/component/list/item/list-item.interface.tsx
+++ src/core/component/list/item/list-item.interface.tsx
@@ -1,9 +1,9 @@
 import { IRippleInternalProps } from '../../../component/ripple';
-import { IEntity, IRenderable } from '../../../definition.interface';
+import { AnyT, IEntity, IRenderable } from '../../../definition.interface';
 
 export interface IListItemOptions extends IRenderable {
   itemIcon?: string;
-  itemValue?(entity: IEntity): string;
+  itemValue?(entity: IEntity): AnyT;
 }
 
 export interface IListItemInternalProps extends IRippleInternalProps,
