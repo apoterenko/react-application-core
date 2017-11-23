a0630f39684dcbe3fa2062a51578f3fc9a2cc486
apoterenko <a.poterenko@gmail.com>
2017-11-23 20:13:40 +0300

    *cond.ts: orDefault util fn

--- src/core/util/cond.ts
+++ src/core/util/cond.ts
@@ -3,3 +3,7 @@ import { AnyT } from '../definition.interface';
 export function orNull<TResult>(condition: AnyT, result: TResult): TResult {
   return condition ? result : null;
 }
+
+export function orDefault(cond: boolean, v: AnyT, defaultV: AnyT): AnyT {
+  return cond ? v : defaultV;
+}
