a6ceb17a91d776a0cfa2bea7c3785bf50cc8ce25
apoterenko <a.poterenko@gmail.com>
2017-12-05 18:47:30 +0300

    *npm run scss (rac.min.css + rac-material.min.css)

--- scripts/scss.js
+++ scripts/scss.js
@@ -2,16 +2,23 @@ var sass = require('node-sass');
 var path = require('path');
 var fs = require('fs');
 
-var result = sass.renderSync({
-  file: 'src/core/component/component.scss',
-  outputStyle: 'compressed',
-  sourceMap: true,
-  importer: function importer(url, prev, done) {
-    if (url[0] === '~') {
-      url = path.resolve('node_modules/', url.substr(1));
+var compile = function (inFile, outFile) {
+  var result = sass.renderSync({
+    file: inFile,
+    outputStyle: 'compressed',
+    sourceMap: true,
+    importer: function importer(url, prev, done) {
+      if (url[0] === '~') {
+        url = path.resolve('node_modules/', url.substr(1));
+      }
+      if (url.indexOf('@material') === 0) {
+        url = path.resolve('node_modules/', url);
+      }
+      return {file: url};
     }
-    return {file: url};
-  }
-});
+  });
+  fs.writeFileSync(outFile, result.css);
+};
 
-fs.writeFileSync('react-application-core.css', result.css);
+compile('src/core/component/component.scss', 'rac.min.css');
+compile('src/core/component/material/material.scss', 'rac-material.min.css');
