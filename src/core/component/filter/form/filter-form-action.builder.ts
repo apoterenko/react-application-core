2dfc60bbcc88d2c6b13620518306b4e8d464580e
apoterenko <a.poterenko@gmail.com>
2017-12-08 22:50:27 +0300

    *filter-form-action.builder: use abstract filter mappers

--- src/core/component/filter/form/filter-form-action.builder.ts
+++ src/core/component/filter/form/filter-form-action.builder.ts
@@ -1,20 +1,28 @@
 import { FilterActionEnum } from '../filter.interface';
 import { BuildListAndFilterFormWrapperStateT } from './filter-form-action.interface';
-import { IApplicationFilterFormWrapperState } from '../../../component/filter';
+import {
+  activeClassNameFilterFormWrapperMapper,
+  disabledActionsListWrapperMapper,
+  IApplicationFilterAction,
+  IApplicationFilterFormWrapperState,
+  IApplicationFilterOptions,
+} from '../../../component/filter';
 
-export const buildActiveFilterFormActionConfig = (state: IApplicationFilterFormWrapperState) => ({
-  type: FilterActionEnum.OPEN_FILTER,
-  className: state.filterForm.dirty && 'rac-filter-active',
-});
+export const buildActiveFilterAction =
+    (state: IApplicationFilterFormWrapperState): IApplicationFilterAction => ({
+      type: FilterActionEnum.OPEN_FILTER,
+      ...activeClassNameFilterFormWrapperMapper(state),
+    });
 
-export const buildRefreshFilterFormOptions = (state: BuildListAndFilterFormWrapperStateT) => {
-  const filterFormState = state as IApplicationFilterFormWrapperState;
-  return {
-    fieldActions: filterFormState.filterForm
-        ? [buildActiveFilterFormActionConfig(filterFormState)]
-        : [],
-    noSearchField: true,
-    disabledActions: state.list.progress,
-    searchIcon: 'refresh',
-  };
-};
+export const buildRefreshFilterFormOptions =
+    (state: BuildListAndFilterFormWrapperStateT): IApplicationFilterOptions => {
+      const filterFormState = state as IApplicationFilterFormWrapperState;
+      return {
+        fieldActions: filterFormState.filterForm
+            ? [buildActiveFilterAction(filterFormState)]
+            : [],
+        noSearchField: true,
+        searchIcon: 'refresh',
+        ...disabledActionsListWrapperMapper(state),
+      };
+    };
