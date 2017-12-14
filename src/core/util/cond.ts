1c04c8f2ba65881309a290716870b73c6881aecf
apoterenko <a.poterenko@gmail.com>
2017-12-14 22:03:23 +0300

    *cond util fix

--- src/core/util/cond.ts
+++ src/core/util/cond.ts
@@ -1,7 +1,12 @@
 import { AnyT } from '../definition.interface';
+import { isFn } from '../util';
 
-export function orNull<TResult>(condition: AnyT, result: TResult): TResult {
-  return condition ? result : null;
+export function orNull<TResult>(condition: AnyT, result: TResult|(() => TResult)): TResult {
+  return condition ? (isFn(result) ? (result as () => TResult)() : result as TResult) : null;
+}
+
+export function orUndef<TResult>(condition: AnyT, result: TResult|(() => TResult)): TResult {
+  return condition ? (isFn(result) ? (result as () => TResult)() : result as TResult) : undefined;
 }
 
 export function orDefault(cond: boolean, v: AnyT, defaultV: AnyT): AnyT {
