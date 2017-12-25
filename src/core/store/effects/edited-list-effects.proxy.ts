c6074ae4414aac689694b77f597c795a168ca1a8
apoterenko <a.poterenko@gmail.com>
2017-12-25 21:40:21 +0300

    *makeEditedListEffectsProxy: $onCloneEntity

--- src/core/store/effects/edited-list-effects.proxy.ts
+++ src/core/store/effects/edited-list-effects.proxy.ts
@@ -1,41 +1,54 @@
 import { EffectsService, IEffectsAction } from 'redux-effects-promise';
+import * as R from 'ramda';
 
 import { provideInSingleton } from '../../di';
 import { FilterActionBuilder } from '../../component/filter';
 import { ListActionBuilder } from '../../component/list';
 import { RouterActionBuilder } from '../../router';
 import { ApplicationStateT } from '../../store';
-import { IEntity, ISelectable } from '../../definition.interface';
+import { CustomActionBuilder } from '../../action';
+import { ID_FIELD_NAME, IEntity, ISelectable } from '../../definition.interface';
+import { FormActionBuilder } from '../../component/form';
 
 export function makeEditedListEffectsProxy<TEntity extends IEntity,
-                                           TApplicationState extends ApplicationStateT>(
-  config: {
-    section: string;
-    pathResolver(entity?: TEntity, state?: TApplicationState): string;
-  }
-): () => void {
-  const {pathResolver, section} = config;
+                                           TApplicationState extends ApplicationStateT>(config: {
+  listSection: string;
+  formSection?: string;
+  pathResolver(entity?: TEntity, state?: TApplicationState): string;
+  entityResolver?(state: TApplicationState): TEntity;
+}): () => void {
+  const {pathResolver, entityResolver, listSection, formSection} = config;
   return (): void => {
 
     @provideInSingleton(Effects)
     class Effects {
 
-      @EffectsService.effects(ListActionBuilder.buildCreateActionType(section))
+      @EffectsService.effects(ListActionBuilder.buildCreateActionType(listSection))
       public $onCreateEntity(_: IEffectsAction, state: TApplicationState): IEffectsAction[] {
         return [
-          ListActionBuilder.buildLockAction(section),    // Prevent the list auto destroying
-          FilterActionBuilder.buildLockAction(section),  // Prevent the list filter auto destroying
+          ListActionBuilder.buildLockAction(listSection),    // Prevent the list auto destroying
+          FilterActionBuilder.buildLockAction(listSection),  // Prevent the list filter auto destroying
           RouterActionBuilder.buildNavigateAction(pathResolver(null, state))
         ];
       }
 
-      @EffectsService.effects(ListActionBuilder.buildSelectActionType(section))
+      @EffectsService.effects(ListActionBuilder.buildSelectActionType(listSection))
       public $onSelectEntity(action: IEffectsAction, state: TApplicationState): IEffectsAction[] {
         const actionParams: ISelectable<TEntity> = action.data;
         return [
-          ListActionBuilder.buildLockAction(section),    // Prevent the list auto destroying
-          FilterActionBuilder.buildLockAction(section),  // Prevent the list filter auto destroying
+          ListActionBuilder.buildLockAction(listSection),    // Prevent the list auto destroying
+          FilterActionBuilder.buildLockAction(listSection),  // Prevent the list filter auto destroying
           RouterActionBuilder.buildNavigateAction(pathResolver(actionParams.selected, state))
+        ];
+      }
+
+      @EffectsService.effects(CustomActionBuilder.buildCustomCloneActionType(formSection))
+      public $onCloneEntity(_: IEffectsAction, state: TApplicationState): IEffectsAction[] {
+        return [
+          FormActionBuilder.buildChangesAction(formSection,
+              R.pickBy((value, key) => key !== ID_FIELD_NAME, entityResolver(state))),
+          ListActionBuilder.buildDeselectAction(listSection),
+          RouterActionBuilder.buildReplaceAction(pathResolver(null, state))
         ];
       }
     }
