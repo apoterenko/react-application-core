3178215b3dc5b3724abf00897468b1a921d552e7
apoterenko <a.poterenko@gmail.com>
2017-12-26 21:06:13 +0300

    *code formatting

--- src/core/util/merge.ts
+++ src/core/util/merge.ts
@@ -2,16 +2,17 @@ import * as R from 'ramda';
 
 import { IEntity, IKeyValue } from '../definition.interface';
 
-export const mergeDeepLeftAndGetChanges = <TChanges extends IKeyValue>(
-    target: TChanges, source: TChanges
-): TChanges => {
-  const mergedEntity = R.mergeDeepLeft<TChanges, TChanges>(target, source);
-  if (!R.equals<TChanges>(mergedEntity, target)) {
-    return R.pickBy<TChanges, TChanges>((value, key) => Reflect.has(source, key), mergedEntity);
-  }
-  return null;
-};
+export const mergeDeepLeftAndGetChanges =
+    <TChanges extends IKeyValue>(target: TChanges,
+                                 source: TChanges): TChanges => {
+      const mergedEntity = R.mergeDeepLeft<TChanges, TChanges>(target, source);
+      if (!R.equals<TChanges>(mergedEntity, target)) {
+        return R.pickBy<TChanges, TChanges>((value, key) => Reflect.has(source, key), mergedEntity);
+      }
+      return null;
+    };
 
-export const mergeDeepLeftAndGetEntity = <TEntity extends IEntity>(
-    target: TEntity, source: TEntity
-): TEntity => mergeDeepLeftAndGetChanges<TEntity>(target, source);
+export const mergeDeepLeftAndGetEntity =
+    <TEntity extends IEntity>(target: TEntity,
+                              source: TEntity): TEntity =>
+        mergeDeepLeftAndGetChanges<TEntity>(target, source);
