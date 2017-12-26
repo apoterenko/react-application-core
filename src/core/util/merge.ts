e449c8238c3837c42196ff27919162153d51fa49
apoterenko <a.poterenko@gmail.com>
2017-12-26 21:03:50 +0300

    *merge: mergeDeepLeftAndGetEntity + mergeDeepLeftAndGetChanges

--- src/core/util/merge.ts
+++ src/core/util/merge.ts
@@ -1,11 +1,17 @@
 import * as R from 'ramda';
 
-import { IKeyValue } from '../definition.interface';
+import { IEntity, IKeyValue } from '../definition.interface';
 
-export const mergeDeepLeftAndGetChanges = (target: IKeyValue, source: IKeyValue): IKeyValue => {
-  const mergedEntity = R.mergeDeepLeft(target, source);
-  if (!R.equals(mergedEntity, target)) {
-    return R.pickBy((value, key) => Reflect.has(source, key), mergedEntity);
+export const mergeDeepLeftAndGetChanges = <TChanges extends IKeyValue>(
+    target: TChanges, source: TChanges
+): TChanges => {
+  const mergedEntity = R.mergeDeepLeft<TChanges, TChanges>(target, source);
+  if (!R.equals<TChanges>(mergedEntity, target)) {
+    return R.pickBy<TChanges, TChanges>((value, key) => Reflect.has(source, key), mergedEntity);
   }
   return null;
 };
+
+export const mergeDeepLeftAndGetEntity = <TEntity extends IEntity>(
+    target: TEntity, source: TEntity
+): TEntity => mergeDeepLeftAndGetChanges<TEntity>(target, source);
