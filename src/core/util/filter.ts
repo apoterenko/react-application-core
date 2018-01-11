15dca516524ca90d9afca020cb10c07fd738b5d9
apoterenko <a.poterenko@gmail.com>
2018-01-11 15:49:11 +0300

    noUndefValuesFilter

--- src/core/util/filter.ts
+++ src/core/util/filter.ts
@@ -1,15 +1,28 @@
 import * as R from 'ramda';
 
 import { AnyT, IKeyValue } from '../definition.interface';
+import { isUndef } from './type';
 
-export function filterBy(source: IKeyValue,
-                         predicate: (key: string, value: AnyT) => boolean): IKeyValue {
-  return {
-    ...R.pickBy((value, key) => {
-      if (typeof value === 'object' && !R.isNil(value) && !Array.isArray(value)) {
-        source[key] = filterBy(value, predicate);
-      }
-      return predicate(key, value);
-    }, source),
-  };
+type PredicateT = (key: string, value: AnyT) => boolean;
+
+export function filterBy<TSource extends IKeyValue, TResult extends IKeyValue>(
+    source: TSource, predicate: PredicateT
+): TResult {
+  return R.pickBy<TSource, TResult>((value, key) => {
+    if (typeof value === 'object' && !R.isNil(value) && !Array.isArray(value)) {
+      source[key] = filterBy(value, predicate);
+    }
+    return predicate(key, value);
+  }, source);
+}
+
+export function filterByPredicate<TSource extends IKeyValue, TResult extends IKeyValue>(
+    source: TSource, predicate: PredicateT
+): TResult {
+  return R.pickBy<TSource, TResult>((value, key) => predicate(key, value), source);
+}
+
+export function noUndefValuesFilter<TSource extends IKeyValue,
+                                    TResult extends IKeyValue>(source: TSource): TResult {
+  return filterByPredicate(source, (key, value) => !isUndef(value));
 }
