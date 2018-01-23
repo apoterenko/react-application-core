db965abdddd83e6990f8564f6ff186b7ad0f3360
apoterenko <a.poterenko@gmail.com>
2018-01-23 21:53:58 +0300

    util: nvl

--- src/core/util/nvl.ts
+++ src/core/util/nvl.ts
@@ -1,6 +1,11 @@
 import * as R from 'ramda';
+
 import { AnyT } from '../definition.interface';
 
 export function nvlEmpty(v: AnyT): AnyT {
   return R.isNil(v) || R.isEmpty(v) ? null : v;
+}
+
+export function nvl(v1: AnyT, v2: AnyT): AnyT {
+  return R.isNil(v1) ? v2 : v1;
 }
