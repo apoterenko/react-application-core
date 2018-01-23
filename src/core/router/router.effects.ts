58cbf38d428b9f6c18b2dbf65510b40e8b7f9529
apoterenko <a.poterenko@gmail.com>
2018-01-23 20:48:31 +0300

    router.effects: logging

--- src/core/router/router.effects.ts
+++ src/core/router/router.effects.ts
@@ -1,5 +1,6 @@
 import { IEffectsAction, EffectsService } from 'redux-effects-promise';
 import { History } from 'history';
+import { LoggerFactory } from 'ts-smart-logger';
 
 import { DI_TYPES, lazyInject, provideInSingleton } from '../di';
 import {
@@ -11,23 +12,34 @@ import { RouterActionBuilder } from './router-action.builder';
 
 @provideInSingleton(RouterEffects)
 export class RouterEffects {
+  private static logger = LoggerFactory.makeLogger(RouterEffects);
 
   @lazyInject(DI_TYPES.Router) private router: IRouter;
 
   @EffectsService.effects(ROUTER_NAVIGATE_ACTION_TYPE)
   public $onNavigate(action: IEffectsAction): void {
     const pathAndState = this.toPathAndState(action);
+
+    RouterEffects.logger.debug(
+        `[$RouterEffects][$onNavigate] Path: ${pathAndState.path}, state: ${pathAndState.state}`
+    );
     this.router.push(pathAndState.path, pathAndState.state);
   }
 
   @EffectsService.effects(ROUTER_REPLACE_ACTION_TYPE)
   public $onReplace(action: IEffectsAction): void {
     const pathAndState = this.toPathAndState(action);
+
+    RouterEffects.logger.debug(
+        `[$RouterEffects][$onReplace] Path: ${pathAndState.path}, state: ${pathAndState.state}`
+    );
     this.router.replace(pathAndState.path, pathAndState.state);
   }
 
   @EffectsService.effects(RouterActionBuilder.buildNavigateBackActionType())
   public $onBack(): void {
+    RouterEffects.logger.debug('[$RouterEffects][$onBack]');
+
     this.router.goBack();
   }
 
