dc4d6a0228b8152fa1ab0beea8c330d0753e875f
apoterenko <a.poterenko@gmail.com>
2018-01-30 15:27:30 +0300

    orDefault fix

--- src/core/util/cond.ts
+++ src/core/util/cond.ts
@@ -9,6 +9,10 @@ export function orUndef<TResult>(condition: AnyT, result: TResult|(() => TResult
   return condition ? (isFn(result) ? (result as () => TResult)() : result as TResult) : undefined;
 }
 
-export function orDefault(cond: boolean, v: AnyT, defaultV: AnyT): AnyT {
-  return cond ? v : defaultV;
+export function orDefault<TResult1, TResult2>(cond: boolean,
+                                              result1: TResult1|(() => TResult1),
+                                              result2: TResult2|(() => TResult2)): TResult1|TResult2 {
+  return cond
+      ? (isFn(result1) ? (result1 as () => TResult1)() : result1 as TResult1)
+      : (isFn(result2) ? (result2 as () => TResult2)() : result2 as TResult2);
 }
