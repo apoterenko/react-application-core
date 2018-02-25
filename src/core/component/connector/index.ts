2ad4c2accd39ed21454f984486e5e9db736228f3
apoterenko <a.poterenko@gmail.com>
2018-02-25 19:02:21 +0300

    remove deprecated connector-builder.action

--- src/core/component/connector/index.ts
+++ src/core/component/connector/index.ts
@@ -2,4 +2,3 @@ export * from './connector.interface';
 export * from './connector.factory';
 export * from './connector.mapper';
 export * from './connector.decorator';
-export * from './connector-builder.action';
