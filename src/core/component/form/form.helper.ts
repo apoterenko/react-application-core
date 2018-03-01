64a1938f6157b467e52a4ff587fcfddc6360cf51
apoterenko <a.poterenko@gmail.com>
2018-03-01 21:49:39 +0300

    form.helper: toFieldModifyPayload

--- src/core/component/form/form.helper.ts
+++ src/core/component/form/form.helper.ts
@@ -1,5 +1,10 @@
 import { IFormEntityWrapper, AnyT } from '../../definition.interface';
 import { isDef } from '../../util';
+import {
+  IFormFieldModifyPayload,
+  IFormFieldsModifyPayload,
+  FormModifyPayloadT,
+} from './form.interface';
 
 export const fromFormChanges = <TEntity>(formEntityWrapper: IFormEntityWrapper<TEntity>,
                                          fieldValueResolver: (changes: TEntity) => AnyT) =>
@@ -8,3 +13,16 @@ export const fromFormChanges = <TEntity>(formEntityWrapper: IFormEntityWrapper<T
 export const isFormChangesExist = <TEntity>(formEntityWrapper: IFormEntityWrapper<TEntity>,
                                             fieldValueResolver: (changes: TEntity) => AnyT) =>
   isDef(fromFormChanges(formEntityWrapper, fieldValueResolver));
+
+export const toFieldModifyPayload = (payload: FormModifyPayloadT,
+                                     fieldName: string): IFormFieldModifyPayload => {
+  const payloadAsFieldModifyPayload = payload as IFormFieldModifyPayload;
+  const payloadAsFieldsModifyPayload = payload as IFormFieldsModifyPayload;
+  const fields: IFormFieldModifyPayload[] = []
+    .concat(
+      payloadAsFieldsModifyPayload.fields
+        ? payloadAsFieldsModifyPayload.fields
+        : [payloadAsFieldModifyPayload]
+    );
+  return fields.find((fieldPayload) => fieldName === fieldPayload.field);
+};
