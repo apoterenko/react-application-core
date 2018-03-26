d1c3a90f0e07f7c971c18d526021b190d46ba3b0
apoterenko <a.poterenko@gmail.com>
2018-03-26 22:35:43 +0300

    introduce env app namespace

--- src/core/env/env.interface.ts
+++ src/core/env/env.interface.ts
@@ -2,6 +2,7 @@ const origin = (location.origin || (location.protocol + '//' + location.host));
 
 export const APP_VERSION = process.env.APP_VERSION;
 export const APP_PROFILE = process.env.APP_PROFILE;
+export const APP_NAMESPACE = process.env.APP_NAMESPACE;
 export const PROD_MODE = process.env.NODE_ENV === 'production';
 export const GOOGLE_KEY = process.env.GOOGLE_KEY;
 export const BASE_PATH = (document.baseURI || location.href).replace(origin, '');
