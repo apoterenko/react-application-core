7644423ab85128c55ffda66575e221642c277bb3
apoterenko <a.poterenko@gmail.com>
2018-03-27 22:39:22 +0300

    json: parse json

--- src/core/util/json.ts
+++ src/core/util/json.ts
@@ -1,3 +1,11 @@
 import { IKeyValue } from '../definition.interface';
 
 export const formatJson = (o: IKeyValue) => JSON.stringify(o, null, 4);
+
+export const parseJson = (o: string): IKeyValue => {
+  try {
+    return JSON.parse(o);
+  } catch (e) {
+    return null;
+  }
+};
