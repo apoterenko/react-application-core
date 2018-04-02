6da8b03181e67e4eda8247f061f0f4b50601b6d3
alex.poter <a.poterenko@gmail.com>
2018-04-02 16:58:09 +0300

    env supports react-native

--- src/core/env/env.interface.ts
+++ src/core/env/env.interface.ts
@@ -1,10 +1,17 @@
-const origin = (location.origin || (location.protocol + '//' + location.host));
+const definedLocation = typeof location === 'undefined'
+  ? { origin: '', protocol: 'http', host: 'localhost', port: '80', href: '' }
+  : location;
+const definedWindow = typeof window === 'undefined' ? {} : window;
+const definedDocument = typeof document === 'undefined' ? { baseURI: '' } : document;
 
+const origin = definedLocation.origin || [definedLocation.protocol, definedLocation.host].join('//');
 export const APP_VERSION = process.env.APP_VERSION;
 export const APP_PROFILE = process.env.APP_PROFILE;
 export const APP_NAMESPACE = process.env.APP_NAMESPACE;
 export const PROD_MODE = process.env.NODE_ENV === 'production';
 export const GOOGLE_KEY = process.env.GOOGLE_KEY;
-export const BASE_PATH = (document.baseURI || location.href).replace(origin, '');
-export const PORT = location.port || '80';
+export const RN_PLATFORM = Reflect.get(definedWindow, '$$RN_PLATFORM');
+export const RN_MODE_ENABLED = !!RN_PLATFORM;
+export const BASE_PATH = (definedDocument.baseURI || definedLocation.href).replace(origin, '');
+export const PORT = definedLocation.port || '80';
 export const NORMALIZED_BASE_PATH = BASE_PATH.replace(/\//g, '');
