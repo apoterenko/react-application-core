9fc85409ff9bc27ce01868993dc1ca572b455388
alex.poter <a.poterenko@gmail.com>
2018-04-05 02:23:07 +0300

    add grid-column.interface

---
+++ src/core/component/grid/column/grid-column.interface.ts
@@ -0,0 +1,7 @@
+import { IBaseContainerInternalProps } from '../../base';
+import { IGridColumnConfiguration } from '../../../configurations-definitions.interface';
+
+/* @stable - 05.04.2018 */
+export interface IGridColumnInternalProps extends IBaseContainerInternalProps,
+                                                  IGridColumnConfiguration {
+}
