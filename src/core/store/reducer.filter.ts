2d09817c11d7713104f5e772721b32b90497a3ba
alex.poter <a.poterenko@gmail.com>
2018-04-10 22:17:43 +0300

    add filterBySection fn

--- src/core/store/reducer.filter.ts
+++ src/core/store/reducer.filter.ts
@@ -5,11 +5,30 @@ import { toSection } from '../util';
 
 export type Filter = (action: IEffectsAction) => boolean;
 
-export function filter<S>(reducer: Reducer<S>, filterObject: Filter): Reducer<S> {
-  return (state, action) => (filterObject(action) ? reducer(state, action) : state);
-}
-
+/**
+ * @stable - 10.04.2018
+ * @param {string} customSection
+ * @returns {Filter}
+ */
 export function reducerSectionFilter(customSection: string): Filter {
   let section;
   return (action) => (!(section = toSection(action)) || customSection === section);
 }
+
+/**
+ * @stable - 10.04.2018
+ * @param {Reducer<S>} reducer
+ * @param {Filter} filterObject
+ * @returns {Reducer<S>}
+ */
+export const filter = <S>(reducer: Reducer<S>, filterObject: Filter): Reducer<S> =>
+  (state, action) => (filterObject(action) ? reducer(state, action) : state);
+
+/**
+ * @stable - 10.04.2018
+ * @param {Reducer<S>} reducer
+ * @param {string} customSection
+ * @returns {Reducer<S>}
+ */
+export const filterBySection = <S>(reducer: Reducer<S>, customSection: string): Reducer<S> =>
+  filter<S>(reducer, reducerSectionFilter(customSection));
