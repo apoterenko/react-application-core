006234db8f15a8e09010e6d14a0fe3334d3d6e26
alex.poter <a.poterenko@gmail.com>
2018-04-14 01:17:42 +0300

    update script

--- scripts/rn-node-compatibility-replace.js
+++ scripts/rn-node-compatibility-replace.js
@@ -1,13 +1,20 @@
 var replace = require("replace");
+var fs = require('fs');
+var file1 = 'node_modules/@types/node/index.d.ts';
+var file2 = 'node_modules/@types/react-native/index.d.ts';
 
-replace({
-  regex: "declare var require: NodeRequire;",
-  replacement: "",
-  paths: ['node_modules/@types/node/index.d.ts'],
-});
+if (fs.existsSync(file1)) {
+  replace({
+    regex: "declare var require: NodeRequire;",
+    replacement: "",
+    paths: [file1],
+  });
+}
 
-replace({
-  regex: "readonly geolocation: Geolocation;",
-  replacement: "",
-  paths: ['node_modules/@types/react-native/index.d.ts'],
-});
+if (fs.existsSync(file2)) {
+  replace({
+    regex: "readonly geolocation: Geolocation;",
+    replacement: "",
+    paths: [file2],
+  });
+}
