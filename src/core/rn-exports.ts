2259688a5cfc409c7b28f6331edddfc7bd1b877d
alex.poter <a.poterenko@gmail.com>
2018-04-14 00:31:25 +0300

    add rn-exports.ts

---
+++ src/core/rn-exports.ts
@@ -0,0 +1,6 @@
+export * from './di';
+export { orNull, orDefault, orUndef } from './util/cond';
+export { UniversalBaseContainer } from './component/base/universal-base.container';
+export { RnBaseContainer } from './component/base/rn-base.container';
+export { listSelectedEntityMapper } from './component/connector/universal-connector.mapper';
+export { convertError } from './error/error.converter';
