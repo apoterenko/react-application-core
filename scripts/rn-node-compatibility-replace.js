de03a25e24b62e54843c7872ee46cc134ce1b198
alex.poter <a.poterenko@gmail.com>
2018-04-19 23:51:10 +0300

    native-base compatibility scripts fixes

--- scripts/rn-node-compatibility-replace.js
+++ scripts/rn-node-compatibility-replace.js
@@ -2,6 +2,7 @@ var replace = require("replace");
 var fs = require('fs');
 var file1 = 'node_modules/@types/node/index.d.ts';
 var file2 = 'node_modules/@types/react-native/index.d.ts';
+var file3 = 'node_modules/native-base/index.d.ts';
 
 if (fs.existsSync(file1)) {
   replace({
@@ -16,5 +17,13 @@ if (fs.existsSync(file2)) {
     regex: "readonly geolocation: Geolocation;",
     replacement: "",
     paths: [file2],
+  });
+}
+
+if (fs.existsSync(file3)) {
+  replace({
+    regex: "ref\\?: React.Ref\\<ReactNative.ViewProperties \\| ReactListViewProperties\\>;",
+    replacement: "ref?: any;",
+    paths: [file3],
   });
 }
