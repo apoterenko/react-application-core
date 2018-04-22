dfb0cde852c3d3f16cd2fac02c8e5bd47555fc8d
alex.poter <a.poterenko@gmail.com>
2018-04-22 23:02:27 +0300

    define $$RN_PLATFORM

--- src/core/rn-polyfills.ts
+++ src/core/rn-polyfills.ts
@@ -1,1 +1,7 @@
 import './polyfills/universal-polyfills';
+
+// Define RN platform
+import { Platform } from 'react-native';
+if (typeof window !== 'undefined') {
+  Reflect.set(window, '$$RN_PLATFORM', Platform.OS);
+}
