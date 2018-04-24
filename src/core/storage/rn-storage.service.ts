7dc810cab173f0216e18a9fbc1229573461ed7b0
alex.poter <a.poterenko@gmail.com>
2018-04-25 02:32:33 +0300

    minor fix

--- src/core/storage/rn-storage.service.ts
+++ src/core/storage/rn-storage.service.ts
@@ -3,6 +3,7 @@ import { AsyncStorage } from 'react-native';
 import { IApplicationStorage, STORAGE_KEY_SEPARATOR } from '../storage';
 import { AnyT } from '../definitions.interface';
 
+// TODO Encryption/Decryption
 export class RnStorage implements IApplicationStorage {
 
   constructor(private prefix: string) {
