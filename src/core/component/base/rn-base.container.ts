0f0e9cb04b7a31d5f98974a361d6eea5d309c45e
alex.poter <a.poterenko@gmail.com>
2018-04-25 21:46:46 +0300

    rn-base.container: add dispatchFormChange

--- src/core/component/base/rn-base.container.ts
+++ src/core/component/base/rn-base.container.ts
@@ -1,6 +1,19 @@
+import { AnyT } from '../../definitions.interface';
 import { UniversalBaseContainer } from './universal-base.container';
 import { IRnBaseContainerProps } from './rn-base.interface';
+import { FormActionBuilder } from '../form/form-action.builder';
 
 export class RnBaseContainer<TInternalProps extends IRnBaseContainerProps>
   extends UniversalBaseContainer<TInternalProps> {
+
+  /**
+   * @stable - 25.04.2018
+   * @param {string} fieldName
+   * @param {AnyT} fieldValue
+   */
+  protected dispatchFormChange(fieldName: string, fieldValue?: AnyT): void {
+    this.appStore.dispatch(
+      FormActionBuilder.buildChangeSimpleAction(this.props.sectionName, fieldName, fieldValue)
+    );
+  }
 }
