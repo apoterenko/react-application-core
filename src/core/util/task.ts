496e5e91cc894f9412a35cb6ec594a5cb3e3dab4
alex.poter <a.poterenko@gmail.com>
2018-05-04 10:49:47 +0300

    add task.ts

---
+++ src/core/util/task.ts
@@ -0,0 +1,33 @@
+import * as R from 'ramda';
+
+export class DelayedTask {
+
+  private taskId: number;
+
+  constructor(private task: () => {}, private period: number, private repeat = false) {
+  }
+
+  public start(): void {
+    this.launchTask();
+  }
+
+  public stop(): void {
+    if (R.isNil(this.taskId)) {
+      return;
+    }
+    clearTimeout(this.taskId);
+    this.taskId = null;
+  }
+
+  private launchTask(): void {
+    this.stop();
+    this.taskId = setTimeout(() => this.onLaunchTask(), this.period);
+  }
+
+  private onLaunchTask(): void {
+    this.task.call(null);
+    if (this.repeat) {
+      this.launchTask();
+    }
+  }
+}
