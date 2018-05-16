0b5ab76febf828418e648141ca5d251124606df1
alex.poter <a.poterenko@gmail.com>
2018-05-16 22:26:14 +0300

    page-toolbar.component refactoring

--- src/core/component/toolbar/page/page-toolbar.component.tsx
+++ src/core/component/toolbar/page/page-toolbar.component.tsx
@@ -1,130 +1,77 @@
 import * as React from 'react';
 
-import { orNull, toClassName, pageFromNumber, pageToNumber } from '../../../util';
+import { orDefault, orNull, toClassName, pageFromNumber, pageToNumber } from '../../../util';
 import { FIRST_PAGE } from '../../../definitions.interface';
 import { BaseComponent } from '../../../component/base';
 import { IPageToolbarProps } from './page-toolbar.interface';
 
 export class PageToolbar extends BaseComponent<PageToolbar, IPageToolbarProps> {
 
-  constructor(props: IPageToolbarProps) {
-    super(props);
-    this.onPrevious = this.onPrevious.bind(this);
-    this.onNext = this.onNext.bind(this);
-    this.onFirst = this.onFirst.bind(this);
-    this.onLast = this.onLast.bind(this);
-  }
-
+  /**
+   * @stable [16.05.2018]
+   * @returns {JSX.Element}
+   */
   public render(): JSX.Element {
     const props = this.props;
-    const isPreviousBtnDisabled = this.isPreviousBtnDisabled;
-    const isNextBtnDisabled = this.isNextBtnDisabled;
+    const toNumber = pageToNumber(props);
+    const fromNumber = pageFromNumber(props);
+    const isPreviousBtnDisabled = props.page === FIRST_PAGE;
+    const isNextBtnDisabled = toNumber === props.totalCount;
 
     return (
-        <div className={toClassName(
-                          this.uiFactory.toolbar,
-                          'rac-toolbar',
-                          'rac-page-toolbar',
-                          props.className
-                       )}>
+        <div className={toClassName(this.uiFactory.toolbar, 'rac-toolbar', 'rac-page-toolbar', props.className)}>
           {
-            props.contentDisplay === false
-                ? <div className={toClassName(this.uiFactory.toolbarRow, 'rac-page-toolbar-content')}/>
-                : (
-                    <div className={toClassName(this.uiFactory.toolbarRow, 'rac-page-toolbar-content')}>
-                      <div className='rac-flex-full'>
-                        {props.children}
+            orDefault<JSX.Element, JSX.Element>(
+              props.totalCount > 0,
+              () => (
+                <div className={toClassName(this.uiFactory.toolbarRow, 'rac-page-toolbar-content')}>
+                  <div className='rac-flex-full'>
+                    {props.children}
+                  </div>
+                  <div className='rac-toolbar-pages-info'>
+                    {fromNumber}-{toNumber} {this.t('of')} {props.totalCount}
+                  </div>
+                  {
+                    orNull(
+                      !(isPreviousBtnDisabled && isNextBtnDisabled),
+                      <div>
+                        {
+                          this.uiFactory.makeIcon({
+                            type: 'first_page',
+                            disabled: isPreviousBtnDisabled,
+                            onClick: props.onFirst,
+                          })
+                        }
+                        {
+                          this.uiFactory.makeIcon({
+                            type: 'keyboard_arrow_left',
+                            disabled: isPreviousBtnDisabled,
+                            onClick: props.onPrevious,
+                          })
+                        }
+                        {
+                          this.uiFactory.makeIcon({
+                            type: 'keyboard_arrow_right',
+                            disabled: isNextBtnDisabled,
+                            onClick: props.onNext,
+                          })
+                        }
+                        {
+                          this.uiFactory.makeIcon({
+                            type: 'last_page',
+                            disabled: isNextBtnDisabled,
+                            onClick: props.onLast,
+                          })
+                        }
                       </div>
-                      <div className='rac-toolbar-pages-info'>
-                        {this.fromNumber}-{this.toNumber} {this.t('of')} {props.totalCount}
-                      </div>
-                      {
-                        orNull(
-                            !(isPreviousBtnDisabled && isNextBtnDisabled),
-                            <div className='rac-toolbar-pagination-controls'>
-                              {
-                                this.uiFactory.makeIcon({
-                                  type: 'first_page',
-                                  disabled: isPreviousBtnDisabled,
-                                  onClick: this.onFirst,
-                                })
-                              }
-                              {
-                                this.uiFactory.makeIcon({
-                                  type: 'keyboard_arrow_left',
-                                  disabled: isPreviousBtnDisabled,
-                                  onClick: this.onPrevious,
-                                })
-                              }
-                              {
-                                this.uiFactory.makeIcon({
-                                  type: 'keyboard_arrow_right',
-                                  disabled: isNextBtnDisabled,
-                                  onClick: this.onNext,
-                                })
-                              }
-                              {
-                                this.uiFactory.makeIcon({
-                                  type: 'last_page',
-                                  disabled: isNextBtnDisabled,
-                                  onClick: this.onLast,
-                                })
-                              }
-                            </div>
-                        )
-                      }
-                    </div>
-                )
+                    )
+                  }
+                </div>
+              ),
+              () => <div className={toClassName(this.uiFactory.toolbarRow, 'rac-page-toolbar-content')}/>
+            )
           }
         </div>
     );
-  }
-
-  /**
-   * @stable [09.05.2018]
-   * @returns {number}
-   */
-  private get fromNumber(): number {
-    return pageFromNumber(this.props);
-  }
-
-  /**
-   * @stable [09.05.2018]
-   * @returns {number}
-   */
-  private get toNumber(): number {
-    return pageToNumber(this.props);
-  }
-
-  private get isPreviousBtnDisabled(): boolean {
-    return this.props.page === FIRST_PAGE;
-  }
-
-  private get isNextBtnDisabled(): boolean {
-    return this.toNumber === this.props.totalCount;
-  }
-
-  private onPrevious(): void {
-    if (this.props.onPrevious) {
-      this.props.onPrevious();
-    }
-  }
-
-  private onNext(): void {
-    if (this.props.onNext) {
-      this.props.onNext();
-    }
-  }
-
-  private onFirst(): void {
-    if (this.props.onFirst) {
-      this.props.onFirst();
-    }
-  }
-
-  private onLast(): void {
-    if (this.props.onLast) {
-      this.props.onLast();
-    }
   }
 }
