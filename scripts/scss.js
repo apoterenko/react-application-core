5750360d6878956daf8bb8ded36c2cade2e77496
alex.poter <a.poterenko@gmail.com>
2018-05-29 19:21:34 +0300

    assets

--- scripts/scss.js
+++ scripts/scss.js
@@ -1,6 +1,7 @@
 var sass = require('node-sass');
 var path = require('path');
 var fs = require('fs');
+var ncp = require('ncp').ncp;
 
 var compile = function (inFile, outFile) {
   var result = sass.renderSync({
@@ -19,6 +20,14 @@ var compile = function (inFile, outFile) {
   });
   fs.writeFileSync(outFile, result.css);
 };
+
+ncp.limit = 16;
+
+ncp('src/core/assets', 'assets', function (err) {
+    if (err) {
+        return console.error(err);
+    }
+});
 
 compile('src/core/bootstrap.scss', 'rac.min.css');
 compile('src/core/material.bootstrap.scss', 'rac-material.min.css');
