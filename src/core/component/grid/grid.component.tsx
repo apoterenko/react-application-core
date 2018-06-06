d3fad0ae53c70e410c2364cb953f21280139d6ca
alex.poter <a.poterenko@gmail.com>
2018-06-07 02:14:20 +0300

    grid.component: local filtering

--- src/core/component/grid/grid.component.tsx
+++ src/core/component/grid/grid.component.tsx
@@ -4,21 +4,28 @@ import * as R from 'ramda';
 import { IGridProps, IFieldProps } from '../../props-definitions.interface';
 import { IGridColumnConfiguration } from '../../configurations-definitions.interface';
 import { ISortDirectionEntity, IFieldChangeEntity } from '../../entities-definitions.interface';
-import { IEntity, AnyT } from '../../definitions.interface';
-import { toClassName, isDef, orNull } from '../../util';
+import { IEntity, AnyT, UNDEF } from '../../definitions.interface';
+import { toClassName, isDef, orNull, isFn } from '../../util';
 import { Checkbox } from '../field';
 import { GridHeaderColumn } from './header';
 import { GridColumn } from './column';
 import { BaseList } from '../list';
 import { GridRow } from './row';
 import { Field } from '../field';
+import { IGridState } from './grid.interface';
 
-export class Grid extends BaseList<Grid, IGridProps> {
+export class Grid extends BaseList<Grid, IGridProps, IGridState> {
 
+  /**
+   * @stable [07.06.2018]
+   * @param {IGridProps} props
+   */
   constructor(props: IGridProps) {
     super(props);
     this.onHeaderColumnClick = this.onHeaderColumnClick.bind(this);
     this.onSettingsClick = this.onSettingsClick.bind(this);
+
+    this.state = {filterChanges: {}};
   }
 
   /**
@@ -62,13 +69,39 @@ export class Grid extends BaseList<Grid, IGridProps> {
     );
   }
 
-  private getFilterColumn(column: IGridColumnConfiguration): React.ReactNode {
+  /**
+   * @stable [07.06.2018]
+   * @returns {IEntity[]}
+   */
+  protected filterAndSortOriginalDataSourceUsingLocalFiltersAndSorters(): IEntity[] {
     const props = this.props;
-    if (column.useGrouping) {
-      return null;
+    const state = this.state;
+    const source = super.filterAndSortOriginalDataSourceUsingLocalFiltersAndSorters();
+
+    if (!R.isNil(source)) {
+      if (props.useLocalFiltering) {
+
+        const filterChanges = state.filterChanges;
+        const columns = this.columnsConfiguration;
+        const changedColumns = Object.keys(filterChanges);
+
+        const defaultLocalFilter = ({query, columnName, entity}) =>
+          R.isNil(query) || R.isEmpty(query) || entity[columnName].toUpperCase().includes(query.toUpperCase());
+
+        if (changedColumns.length > 0) {
+          return source.filter((entity) => {
+            let result = true;
+            changedColumns.forEach((columnName) => {
+              const query = filterChanges[columnName];
+              const column = columns.find((column0) => column0.name === columnName);
+              result = result && (isFn(column.localFilter) ? column.localFilter : defaultLocalFilter)({query, columnName, entity});
+            });
+            return result;
+          });
+        }
+      }
     }
-    // TODO
-    return '..';
+    return source;
   }
 
   private onSettingsClick(): void {
@@ -81,19 +114,63 @@ export class Grid extends BaseList<Grid, IGridProps> {
    */
   private onHeaderColumnClick(payload: ISortDirectionEntity): void {
     const props = this.props;
-    if (props.onChangeSortDirection) {
-      props.onChangeSortDirection(payload);
+    if (props.onChangeSorting) {
+      props.onChangeSorting(payload);
     }
   }
 
   /**
-   * @stable - 05.04.2018
+   * @stable [06.06.2018]
    * @param {IFieldChangeEntity} payload
    */
-  private onChangeField(payload: IFieldChangeEntity): void {
+  private onChangeRowField(payload: IFieldChangeEntity): void {
     const props = this.props;
+
     if (props.onChange) {
       props.onChange(payload);
+    }
+  }
+
+  /**
+   * @stable [07.06.2018]
+   * @param {IFieldChangeEntity} payload
+   */
+  private onChangeGroupingRowField(payload: IFieldChangeEntity): void {
+    const props = this.props;
+
+    if (props.onChangeGrouping) {
+      props.onChangeGrouping(payload);
+    }
+  }
+
+  /**
+   * @stable [07.06.2018]
+   * @param {IFieldChangeEntity} payload
+   */
+  private onChangeHeaderField(payload: IFieldChangeEntity): void {
+    const props = this.props;
+
+    if (props.onChangeHeader) {
+      props.onChangeHeader(payload);
+    }
+  }
+
+  /**
+   * @stable [07.06.2018]
+   * @param {IFieldChangeEntity} payload
+   */
+  private onChangeFilterField(payload: IFieldChangeEntity): void {
+    const props = this.props;
+
+    if (props.useLocalFiltering) {
+      this.setState({
+        filterChanges: {
+          ...this.state.filterChanges,
+          [payload.name]: payload.value,
+        },
+      });
+    } else if (props.onChangeFilter) {
+      props.onChangeFilter(payload);
     }
   }
 
@@ -107,9 +184,10 @@ export class Grid extends BaseList<Grid, IGridProps> {
     if (column.useGrouping) {
       const name = this.toHeaderFieldName(columnNum);
       return (
-        <Checkbox name={name}
+        <Checkbox {...this.getDefaultFieldProps()}
+                  name={name}
                   value={this.toCheckboxFieldValue(name)}
-                  onChange={(value) => this.onChangeField({value, name})}/>
+                  onChange={(value) => this.onChangeHeaderField({value, name})}/>
       );
     } else if (column.headerRenderer) {
       return column.headerRenderer(column);
@@ -144,7 +222,7 @@ export class Grid extends BaseList<Grid, IGridProps> {
   }
 
   /**
-   * @stable - 06.04.2018
+   * @stable [07.06.2018]
    * @param {IEntity} entity
    * @param {IGridColumnConfiguration} column
    * @param {number} columnNum
@@ -153,31 +231,74 @@ export class Grid extends BaseList<Grid, IGridProps> {
   private getColumn(entity: IEntity,
                     column: IGridColumnConfiguration,
                     columnNum: number): React.ReactNode {
-    const name = this.toFieldName(entity, columnNum);
+    const name = this.toFieldName(entity, column, columnNum);
     if (column.useGrouping) {
       return (
-        <Checkbox name={name}
+        /**
+         * Default group field
+         */
+        <Checkbox {...this.getDefaultFieldProps()}
+                  name={name}
                   value={this.toCheckboxFieldValue(name)}
-                  onChange={(value) => this.onChangeField({value, name})}/>
+                  onChange={(value) => this.onChangeGroupingRowField({value, name, rawData: entity})}/>
       );
     } else if (column.tpl) {
+      /**
+       * Build using a text template
+       */
       return column.tpl(entity);
     } else if (column.renderer) {
+      /**
+       * Build using a renderer
+       */
       const renderEl = column.renderer(entity);
       if (R.isNil(renderEl)) {
         return renderEl;
       }
-      if (Field.isPrototypeOf(renderEl.type)) {
+      if (this.isElementField(renderEl)) {
         return React.cloneElement<IFieldProps>(renderEl, {
-          notUseErrorMessage: true,
-          readOnly: this.props.deactivated,
-          onChange: (value) => this.onChangeField({value, name: column.name || name, rawData: entity}),
+          ...this.getDefaultFieldProps(),
+          onChange: (value) => this.onChangeRowField({value, name, rawData: entity}),
         });
-      } else {
+      }
+      return renderEl;
+    } else if (column.name) {
+      /**
+       * Build by a column name
+       */
+      return Reflect.get(entity, column.name);
+    }
+    return null;
+  }
+
+  /**
+   * @stable [07.06.2018]
+   * @param {IGridColumnConfiguration} column
+   * @param {number} columnNum
+   * @returns {React.ReactNode}
+   */
+  private getFilterColumn(column: IGridColumnConfiguration,
+                          columnNum: number): React.ReactNode {
+    if (column.filterRenderer) {
+      /**
+       * Build using a renderer
+       */
+      const renderEl = column.filterRenderer(column);
+      if (R.isNil(renderEl)) {
         return renderEl;
       }
-    } else if (column.name) {
-      return Reflect.get(entity, column.name);
+      if (this.isElementField(renderEl)) {
+        const name = this.toFilterFieldName(column, columnNum);
+
+        return React.cloneElement<IFieldProps>(renderEl, {
+          ...this.getDefaultFieldProps(),
+          value: this.toFilterFieldValue(name),
+          placeholder: 'Filter',
+          clearAction: true,
+          onChange: (value) => this.onChangeFilterField({value, name}),
+        });
+      }
+      return renderEl;
     }
     return null;
   }
@@ -214,7 +335,7 @@ export class Grid extends BaseList<Grid, IGridProps> {
   private getFilter(): JSX.Element {
     const columnsConfiguration = this.columnsConfiguration;
     const isAtLeastOneFilterExist = columnsConfiguration
-      .filter((column) => column.useLocalFiltering).length > 0;
+      .filter((column) => !R.isNil(column.filterRenderer)).length > 0;
 
     return (
       orNull(
@@ -225,7 +346,7 @@ export class Grid extends BaseList<Grid, IGridProps> {
               columnsConfiguration.map((column, columnNum) => (
                 <GridHeaderColumn key={this.toFilterColumnKey(columnNum)}
                                   {...column}>
-                  {this.getFilterColumn(column)}
+                  {this.getFilterColumn(column, columnNum)}
                 </GridHeaderColumn>
               ))
             }
@@ -236,13 +357,46 @@ export class Grid extends BaseList<Grid, IGridProps> {
   }
 
   /**
-   * @stable - 05.04.2018
+   * @stable [07.06.2018]
+   * @param {string} name
+   * @returns {AnyT}
+   */
+  private toFilterFieldValue(name: string): AnyT {
+    const props = this.props;
+    const state = this.state;
+    return props.useLocalFiltering ? state.filterChanges[name] : UNDEF;
+  }
+
+  /**
+   * @stable [07.06.2018]
+   * @returns {IFieldProps}
+   */
+  private getDefaultFieldProps(): IFieldProps {
+    return {
+      notUseErrorMessage: true,
+      readOnly: this.props.deactivated,
+    };
+  }
+
+  /**
+   * @stable [07.06.2018]
    * @param {IEntity} entity
+   * @param {IGridColumnConfiguration} column
    * @param {number} columnNum
    * @returns {string}
    */
-  private toFieldName(entity: IEntity, columnNum: number): string {
-    return `$$dynamic-field-${entity.id}-${columnNum}`;   // Infinity scroll supporting
+  private toFieldName(entity: IEntity, column: IGridColumnConfiguration, columnNum: number): string {
+    return column.name || `$$dynamic-field-${entity.id}-${columnNum}`;   // Infinity scroll supporting
+  }
+
+  /**
+   * @stable [07.06.2018]
+   * @param {IGridColumnConfiguration} column
+   * @param {number} columnNum
+   * @returns {string}
+   */
+  private toFilterFieldName(column: IGridColumnConfiguration, columnNum: number): string {
+    return column.name || `$$dynamic-filter-field-${columnNum}`;
   }
 
   /**
@@ -292,7 +446,16 @@ export class Grid extends BaseList<Grid, IGridProps> {
   }
 
   /**
-   * @stable - 05.04.2018
+   * @stable [07.06.2018]
+   * @param {JSX.Element} renderEl
+   * @returns {boolean}
+   */
+  private isElementField(renderEl: JSX.Element): boolean {
+    return Field.isPrototypeOf(renderEl.type);
+  }
+
+  /**
+   * @stable [07.06.2018]
    * @returns {IGridColumnConfiguration[]}
    */
   private get columnsConfiguration(): IGridColumnConfiguration[] {
