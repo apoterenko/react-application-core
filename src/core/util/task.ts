8fbb04ab56bc780976a8b1fbae34d3e2f1fa6b38
alex.poter <a.poterenko@gmail.com>
2018-07-31 03:58:31 +0300

    task util: add context

--- src/core/util/task.ts
+++ src/core/util/task.ts
@@ -1,16 +1,33 @@
 import * as R from 'ramda';
 
+import { AnyT } from '../definitions.interface';
+
 export class DelayedTask {
 
   private taskId: number;
+  private context: AnyT;
 
-  constructor(private task: () => void, private period: number, private repeat = false) {
+  /**
+   * @stable [29.07.2018]
+   * @param {(context?: AnyT) => void} task
+   * @param {number} period
+   * @param {boolean} repeat
+   */
+  constructor(private task: (context?: AnyT) => void, private period: number = 0, private repeat = false) {
   }
 
-  public start(): void {
+  /**
+   * @stable [29.07.2018]
+   * @param {AnyT} context
+   */
+  public start(context?: AnyT): void {
+    this.context = context;
     this.launchTask();
   }
 
+  /**
+   * @stable [29.07.2018]
+   */
   public stop(): void {
     if (R.isNil(this.taskId)) {
       return;
@@ -19,15 +36,25 @@ export class DelayedTask {
     this.taskId = null;
   }
 
+  /**
+   * @stable [29.07.2018]
+   */
   private launchTask(): void {
     this.stop();
     this.taskId = setTimeout(() => this.onLaunchTask(), this.period);
   }
 
+  /**
+   * @stable [29.07.2018]
+   */
   private onLaunchTask(): void {
-    this.task.call(null);
+    this.task.call(null, this.context);
+
     if (this.repeat) {
       this.launchTask();
+    } else {
+      this.taskId = null;
+      this.context = null;
     }
   }
 }
