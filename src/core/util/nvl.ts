c11b68f2e66e37e442e69fba41427285a562f3eb
alex.poter <a.poterenko@gmail.com>
2018-08-05 12:22:02 +0300

    util fixes

--- src/core/util/nvl.ts
+++ src/core/util/nvl.ts
@@ -2,14 +2,27 @@ import * as R from 'ramda';
 
 import { AnyT, UNDEF } from '../definitions.interface';
 
-export function nvlEmpty(v: AnyT): AnyT {
-  return R.isNil(v) || R.isEmpty(v) ? null : v;
-}
+/**
+ * @stable [05.08.2018]
+ * @param {AnyT} v
+ * @returns {AnyT}
+ */
+export const undefEmpty = (v: AnyT): AnyT => R.isNil(v) || R.isEmpty(v) ? UNDEF : v;
 
-export function undefEmpty(v: AnyT): AnyT {
-  return R.isNil(v) || R.isEmpty(v) ? UNDEF : v;
-}
+/**
+ * @stable [05.08.2018]
+ * @param {AnyT} v
+ * @returns {AnyT}
+ */
+export const trimmedUndefEmpty = (v: AnyT): AnyT => {
+  const result = undefEmpty(v);
+  return result === UNDEF ? result : String(result).trim();
+};
 
-export function nvl(v1: AnyT, v2: AnyT): AnyT {
-  return R.isNil(v1) ? v2 : v1;
-}
+/**
+ * @stable [05.08.2018]
+ * @param {AnyT} v1
+ * @param {AnyT} v2
+ * @returns {AnyT}
+ */
+export const nvl = (v1: AnyT, v2: AnyT): AnyT => R.isNil(v1) ? v2 : v1;
