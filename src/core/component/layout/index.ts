0c865c169882a2b5b4b10423222aba4e4a16fac9
apoterenko <a.poterenko@gmail.com>
2018-08-11 00:52:13 +0300

    layout fixes

--- src/core/component/layout/index.ts
+++ src/core/component/layout/index.ts
@@ -1,5 +1,4 @@
 export * from './layout.interface';
-export * from './layout.reducer';
 export * from './layout.container';
 export * from './form';
 export * from './default';
--- src/core/component/layout/layout.interface.ts
+++ src/core/component/layout/layout.interface.ts
@@ -1,10 +1,18 @@
 import { ILayoutEntity } from '../../entities-definitions.interface';
+import { ACTION_PREFIX } from '../../definitions.interface';
 
 export const LAYOUT_FULL_MODE = 'full';
 export const LAYOUT_MINIMAL_MODE = 'minimal';
 
+/**
+ * @stable [10.08.2018]
+ * @type {{mode: string}}
+ */
 export const INITIAL_APPLICATION_LAYOUT_STATE: ILayoutEntity = {
+  x: 0,
+  y: 0,
   mode: LAYOUT_FULL_MODE,
 };
 
-export const LAYOUT_MODE_UPDATE_ACTION_TYPE = 'layout.mode.update';
+export const LAYOUT_UPDATE_ACTION_TYPE = `${ACTION_PREFIX}layout.update`;
+export const LAYOUT_DESTROY_ACTION_TYPE = `${ACTION_PREFIX}layout.destroy`;
