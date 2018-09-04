2fbd819dbdb591361feba86cd67f2c23c6dcc398
alex.poter <a.poterenko@gmail.com>
2018-09-04 15:14:24 +0300

    basic-textfield.component fixes

--- src/core/component/field/textfield/basic-textfield.component.tsx
+++ src/core/component/field/textfield/basic-textfield.component.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import * as R from 'ramda';
 import * as $ from 'jquery';
+import * as P from 'platform';
 import MaskedTextInput from 'react-text-mask';
 import { LoggerFactory, ILogger } from 'ts-smart-logger';
 
-import { orNull, toClassName, nvl, cancelEvent } from '../../../util';
-import { IFocusEvent, IBasicEvent, IKeyboardEvent, UNI_CODES } from '../../../definitions.interface';
+import { orNull, toClassName, nvl, cancelEvent, IJqElement } from '../../../util';
+import { IBasicEvent, UNI_CODES } from '../../../definitions.interface';
 import { IFieldActionConfiguration, FieldActionPositionEnum } from '../../../configurations-definitions.interface';
 import { Field, IField } from '../field';
 import { ProgressLabel } from '../../progress';
@@ -15,6 +16,7 @@ import {
   IBasicTextFieldProps,
   IBasicTextField,
 } from './basic-textfield.interface';
+
 export class BasicTextField<TComponent extends IField<TInternalProps, TInternalState>,
                             TInternalProps extends IBasicTextFieldProps,
                             TInternalState extends IBasicTextFieldState>
@@ -32,20 +34,11 @@ export class BasicTextField<TComponent extends IField<TInternalProps, TInternalS
 
   constructor(props: TInternalProps) {
     super(props);
-    this.closeKeyboard = this.closeKeyboard.bind(this);
     this.onWindowMouseDown = this.onWindowMouseDown.bind(this);
 
     if (this.props.clearAction !== false) {
       this.addClearAction();
     }
-  }
-
-  /**
-   * @stable [16.05.2018]
-   */
-  public componentWillUnmount(): void {
-    super.componentWillUnmount();
-    this.onCloseKeyboard();
   }
 
   public render(): JSX.Element {
@@ -64,6 +57,8 @@ export class BasicTextField<TComponent extends IField<TInternalProps, TInternalS
             )}
             <div className={this.getInputElementWrapperClassName()}>
               {this.getInputElement()}
+              {this.getInputMirrorElement()}
+              {this.getInputCaretElement()}
               {
                 orNull(
                     fieldLabel,
@@ -91,7 +86,7 @@ export class BasicTextField<TComponent extends IField<TInternalProps, TInternalS
                   title: action.title && this.t(action.title),
                   className: action.className,
                   disabled: R.isNil(action.disabled)
-                      ? this.isDeactivated()
+                      ? this.isInactive()
                       : action.disabled,
                   onClick: (event: IBasicEvent) => {
                     if (action.onClick) {
@@ -106,31 +101,12 @@ export class BasicTextField<TComponent extends IField<TInternalProps, TInternalS
               () => <ProgressLabel className='rac-text-field-loader'/>
             )}
           </div>
-          {this.fieldMessage}
-          {this.fieldErrorMessageElement}
+          {this.getMessageElement()}
+          {this.getErrorMessageElement()}
           {this.getAttachmentElement()}
-          {
-            orNull<JSX.Element>(
-              props.useKeyboard && this.state.keyboardOpened,
-              () => (
-                <Keyboard ref='keyboard'
-                          field={this.input}
-                          onClose={this.closeKeyboard}
-                          onChange={this.onChangeManually}/>
-              )
-            )
-          }
+          {this.getKeyboardElement()}
         </div>
     );
-  }
-
-  /**
-   * @stable [16.05.2018]
-   * @param {IKeyboardEvent} event
-   */
-  public onKeyTab(event: IKeyboardEvent) {
-    this.closeKeyboard();
-    super.onKeyTab(event);
   }
 
   protected getInputElement(): JSX.Element {
@@ -154,20 +130,15 @@ export class BasicTextField<TComponent extends IField<TInternalProps, TInternalS
     return null;
   }
 
-  protected getInputAttachmentElement(): JSX.Element {
-    return null;
-  }
-
   protected getSelfElementClassName(): string {
     const error = this.error;
     const state = this.state;
 
     return toClassName(
       'rac-text-field',
-      state.keyboardOpened && 'rac-text-field-keyboard-opened',
       this.uiFactory.textField,
       this.uiFactory.textFieldUpgraded,
-      this.isFieldFocused() && this.uiFactory.textFieldFocused,
+      this.isFieldFocused() && this.uiFactory.fieldFocused,
       error && this.uiFactory.textFieldInvalid
     );
   }
@@ -180,25 +151,66 @@ export class BasicTextField<TComponent extends IField<TInternalProps, TInternalS
         this0.clearValue();
       },
       get disabled(): boolean {
-        return this0.isDeactivated() || !this0.isValuePresent();
+        return this0.isInactive() || !this0.isValuePresent();
       },
     };
     this.defaultActions.push(clearAction);
   }
 
   /**
-   * @stable [20.08.2018]
-   * @param {IFocusEvent} event
-   * @returns {boolean}
+   * @stable [03.09.2018]
+   * @returns {JSX.Element}
    */
-  protected onFocus(event: IFocusEvent): boolean {
-    const props = this.props;
-    const result = super.onFocus(event);
-    if (result && props.useKeyboard) {
-      this.removeFocus(); // Prevent native keyboard opening
-      this.openKeyboard();
+  protected toKeyboardElement(): JSX.Element {
+    return (
+      <Keyboard ref='keyboard'
+                field={this.input}
+                onClose={this.closeSyntheticKeyboard}
+                onChange={this.onChangeManually}/>
+    );
+  }
+
+  /**
+   * @stable [20.08.2018]
+   */
+  protected openSyntheticKeyboard(): boolean {
+    const result = super.openSyntheticKeyboard();
+    if (result) {
+      this.eventManager.add(window, 'mousedown', this.onWindowMouseDown);
     }
     return result;
+  }
+
+  /**
+   * @stable [04.09.2018]
+   */
+  protected onCloseKeyboard(): boolean {
+    const result = super.onCloseKeyboard();
+    if (result) {
+      this.eventManager.remove(window, 'mousedown', this.onWindowMouseDown);
+    }
+    return result;
+  }
+
+  /**
+   * @stable [04.09.2018]
+   */
+  protected refreshCaretPosition(): void {
+    super.refreshCaretPosition();
+    this.input.scrollLeft = this.input.scrollWidth;
+  }
+
+  /**
+   * @stable [04.09.2018]
+   * @returns {number}
+   */
+  protected getCaretPosition(): number {
+    const value = this.value;
+    const rightBorderWidth = 2;
+
+    return R.isNil(value) || R.isEmpty(String(value))
+      ? 0
+      : Math.min(this.jMirrorInput.width(), this.jqInput.width() - rightBorderWidth);
   }
 
   /**
@@ -212,49 +224,7 @@ export class BasicTextField<TComponent extends IField<TInternalProps, TInternalS
     if (this.input === targetEl || keyboard.find(targetEl).length !== 0) {
       return;
     }
-    this.closeKeyboard();
-  }
-
-  /**
-   * @stable [16.05.2018]
-   */
-  private closeKeyboard(): void {
-    const props = this.props;
-    if (!props.useKeyboard) {
-      return;
-    }
-    this.setState({keyboardOpened: false});
-    this.onCloseKeyboard();
-
-    BasicTextField.logger.debug(
-      `[$BasicTextField][closeKeyboard] A keyboard has been closed to the field "${props.name}".`
-    );
-  }
-
-  /**
-   * @stable [16.05.2018]
-   */
-  private onCloseKeyboard(): void {
-    if (!this.props.useKeyboard) {
-      return;
-    }
-    this.eventManager.remove(window, 'mousedown', this.onWindowMouseDown);
-  }
-
-  /**
-   * @stable [20.08.2018]
-   */
-  private openKeyboard(): void {
-    const props = this.props;
-    if (this.state.keyboardOpened) {
-      return;
-    }
-    this.setState({keyboardOpened: true});
-    this.eventManager.add(window, 'mousedown', this.onWindowMouseDown);
-
-    BasicTextField.logger.debug(
-      `[$BasicTextField][openKeyboard] A keyboard has been opened to the field "${props.name}".`
-    );
+    this.closeSyntheticKeyboard();
   }
 
   private get actions(): IFieldActionConfiguration[] {
@@ -264,5 +234,51 @@ export class BasicTextField<TComponent extends IField<TInternalProps, TInternalS
     } else {
       return (this.props.actions || []).concat(this.defaultActions || []);
     }
+  }
+
+  /**
+   * @stable [04.09.2018]
+   * @returns {JSX.Element}
+   */
+  private getInputMirrorElement(): JSX.Element {
+    const value = this.value;
+    const content = this.props.type === 'password'
+      ? new Array((value || '').length).join(P.name === 'Safari' ? 'o' : '•')
+      : value;
+
+    return (
+      <span ref='mirrorFieldInput'
+            className={toClassName(this.getInputElementProps().className, 'rac-invisible')}
+            style={{width: 'auto'}}>
+        {content}
+      </span>
+    );
+  }
+
+  /**
+   * @stable [04.09.2018]
+   * @returns {JSX.Element}
+   */
+  private getInputCaretElement(): JSX.Element {
+    const state = this.state;
+    const props = this.props;
+
+    return orNull<JSX.Element>(
+      state.keyboardOpened && !R.isNil(state.caretVisibility),
+      () => (
+        <div className={toClassName('rac-field-input-caret', props.type && `rac-field-input-password-${props.type}`)}
+             style={{visibility: state.caretVisibility ? 'hidden' : 'visible', left: state.caretPosition}}>
+          |
+        </div>
+      )
+    );
+  }
+
+  /**
+   * @stable [04.09.2018]
+   * @returns {IJqElement}
+   */
+  private get jMirrorInput(): IJqElement {
+    return $(this.refs.mirrorFieldInput as HTMLElement);
   }
 }
