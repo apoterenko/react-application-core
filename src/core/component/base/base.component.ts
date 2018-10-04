f3006ef49823bce397ba4e8301b1e94b24de674a
alex.poter <a.poterenko@gmail.com>
2018-10-04 16:01:11 +0300

    cleaning

--- src/core/component/base/base.component.ts
+++ src/core/component/base/base.component.ts
@@ -1,6 +1,4 @@
-import { lazyInject, DI_TYPES } from '../../di';
 import { cancelEvent } from '../../util';
-import { IEventManager } from '../../event';
 import { UniversalComponent } from './universal.component';
 import { IComponent } from '../../entities-definitions.interface';
 import { IComponentProps } from '../../props-definitions.interface';
@@ -11,13 +9,6 @@ export class BaseComponent<TComponent extends IComponent<TProps, TState>,
                            TState = {}>
     extends UniversalComponent<TComponent, TProps, TState>
     implements IComponent<TProps, TState> {
-
-  @lazyInject(DI_TYPES.EventManager) protected eventManager: IEventManager;
-
-  public componentWillReceiveProps(nextProps: Readonly<TProps>, nextContext: {}): void {
-    this.plugins.forEach((plugin) =>
-        plugin.componentWillReceiveProps && plugin.componentWillReceiveProps(nextProps, nextContext));
-  }
 
   public componentWillUpdate(nextProps: Readonly<TProps>, nextState: Readonly<TState>, nextContext: {}): void {
     this.plugins.forEach((plugin) =>
