160d7b4c061dbc22cae6917bacd304336e59424f
alex.poter <a.poterenko@gmail.com>
2018-10-10 22:27:32 +0300

    *vue

--- src/core/component/application/index.ts
+++ src/core/component/application/index.ts
@@ -3,3 +3,4 @@ export * from './application.interface';
 export * from './application.reducer';
 export * from './application-action.builder';
 export * from './universal-application.container';
+export * from './vue-application.container';
