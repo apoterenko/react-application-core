27b4259fbd4bb1df716a7643fcf110da1a7a4c32
alex.poter <a.poterenko@gmail.com>
2018-10-22 02:20:12 +0300

    add vueConnectorOptionsFactory (redux connector)

---
+++ src/core/component/connector/vue-connector.factory.ts
@@ -0,0 +1,89 @@
+import { Unsubscribe } from 'redux';
+
+import { isFn } from '../../util';
+import { UniversalConnectorActionBuilder } from './universal-connector-action.builder';
+import { IKeyValue } from '../../definitions.interface';
+import { VueComponentOptionsT } from '../../vue-definitions.interface';
+import { IVueContainer, IVueApplicationStoreEntity } from '../../vue-entities-definitions.interface';
+import { IVueConnectorOptionsConfigEntity } from './vue-connector.interface';
+
+/**
+ * @stable [22.10.2018]
+ * @param {IVueConnectorOptionsConfigEntity<TApplicationStoreEntity extends IVueApplicationStoreEntity>} config
+ * @returns {VueComponentOptionsT<IVueContainer>}
+ */
+export const vueConnectorOptionsFactory = <TApplicationStoreEntity extends IVueApplicationStoreEntity = IVueApplicationStoreEntity>(
+  config: IVueConnectorOptionsConfigEntity<TApplicationStoreEntity>
+): VueComponentOptionsT<IVueContainer> => {
+
+  let storeUnsubscriber: Unsubscribe;
+  const reduxStoreListeners = [];
+  const customComputed = config.customComputed$;
+
+  if (Array.isArray(customComputed)) {
+    customComputed.forEach((mapper) => reduxStoreListeners.push(mapper));
+  }
+
+  return {
+    /**
+     * @stable [21.10.2018]
+     */
+    computed: config.computed,
+    template: config.template,
+    watch: config.watch,
+
+    /**
+     * @stable [21.10.2018]
+     */
+    data() {
+      const initialData = {};
+      reduxStoreListeners.forEach((mapper) => Object.assign(initialData, mapper(this.state$)));
+
+      return {
+        ...(isFn(config.data) ? (config.data as () => IKeyValue).call(this) : {}),
+        ...initialData, // Vue does require initialization of custom computed properties
+      };
+    },
+
+    /**
+     * @stable [21.10.2018]
+     */
+    beforeCreate() {
+      this.isContainer$ = config.isContainer$ !== false;
+      this.section$ = config.section$;
+    },
+
+    /**
+     * @stable [21.10.2018]
+     */
+    mounted() {
+      if (isFn(config.mounted)) {
+        config.mounted();
+      }
+
+      this.store$.dispatch({type: UniversalConnectorActionBuilder.buildInitActionType(this.section$)});
+
+      storeUnsubscriber = this.store$.subscribe(() => {
+        const state = this.state$;
+
+        /**
+         * A "Redux <-> Container" bridge implemented here.
+         * Vue is updated when the data is updated via assign new links
+         */
+        reduxStoreListeners.forEach((mapper) => Object.assign(this, mapper(state)));
+      });
+    },
+
+    /**
+     * @stable [21.10.2018]
+     */
+    beforeDestroy() {
+      if (isFn(storeUnsubscriber)) {
+        storeUnsubscriber();
+      }
+      if (isFn(config.beforeDestroy)) {
+        config.beforeDestroy();
+      }
+    },
+  };
+};
