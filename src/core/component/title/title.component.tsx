6fdb390ff3f27948d658117a28978b4e610bfc34
alex.poter <a.poterenko@gmail.com>
2018-10-22 03:10:01 +0300

    title.component fixes

--- src/core/component/title/title.component.tsx
+++ src/core/component/title/title.component.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
+import * as R from 'ramda';
 
-import { toClassName } from '../../util';
+import { toClassName, orNull } from '../../util';
 import { BaseComponent } from '../base';
+import { FlexLayout } from '../layout';
+import { ITitleProps } from './title.interface';
 
-export class Title extends BaseComponent<Title> {
+export class Title extends BaseComponent<Title, ITitleProps> {
 
   /**
    * @stable [03.10.2018]
@@ -12,9 +15,25 @@ export class Title extends BaseComponent<Title> {
   public render(): JSX.Element {
     const props = this.props;
     return (
-      <div className={toClassName('rac-title', props.className)}>
-        {this.t(props.children as string)}
-      </div>
+      <FlexLayout full={false}
+                  row={true}
+                  className={toClassName('rac-title-wrapper', props.className)}>
+        <FlexLayout className='rac-title'
+                    justifyContentCenter={true}>
+          {this.t(props.children as string)}
+        </FlexLayout>
+        {
+          orNull<JSX.Element>(
+            !R.isNil(props.items),
+            () => (
+              <FlexLayout full={false}
+                          justifyContentCenter={true}>
+                {props.items}
+              </FlexLayout>
+            )
+          )
+        }
+      </FlexLayout>
     );
   }
 }
