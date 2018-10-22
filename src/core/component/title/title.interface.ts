23464b6a1e639e7b323c7788d82e767f10530fe7
alex.poter <a.poterenko@gmail.com>
2018-10-22 04:16:26 +0300

    title.interface

---
+++ src/core/component/title/title.interface.ts
@@ -0,0 +1,9 @@
+import { IComponentProps } from '../../props-definitions.interface';
+import { IItemsWrapper } from '../../definitions.interface';
+
+/**
+ * @stable [19.10.2018]
+ */
+export interface ITitleProps extends IComponentProps,
+                                     IItemsWrapper<JSX.Element> {
+}
