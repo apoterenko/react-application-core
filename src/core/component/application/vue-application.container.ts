4fd29a24e6848e1a26dff21e52198209e835eccb
alex.poter <a.poterenko@gmail.com>
2018-10-22 03:26:30 +0300

    vue-application.container fixes

--- src/core/component/application/vue-application.container.ts
+++ src/core/component/application/vue-application.container.ts
@@ -1,23 +1,33 @@
-import { CreateElement, VNode } from 'vue';
+import { CreateElement } from 'vue';
 import { Component } from 'vue-property-decorator';
 
 import { DI_TYPES, lazyInject } from '../../di';
-import { IVueContainerEntity } from '../../vue-entities-definitions.interface';
-import { VueBaseContainer } from '../base/vue-base.container';
+import { APPLICATION_SECTION } from '../application/application.interface';
+import { VueNodeT } from '../../vue-definitions.interface';
+import { VueBaseContainer } from '../base/vue-index';
+import { vueConnectorOptionsFactory } from '../connector/vue-index';
+import { applicationMapper } from '../connector/universal-connector.mapper';
 
-@Component
-export class VueApplicationContainer extends VueBaseContainer implements IVueContainerEntity<any> {
+@Component(vueConnectorOptionsFactory({
+  section$: APPLICATION_SECTION,
+  customComputed$: [
+    applicationMapper
+  ],
+}))
+export class VueApplicationContainer extends VueBaseContainer {
 
   @lazyInject(DI_TYPES.Routes) private routes;
 
-  public render(factory: CreateElement): VNode {
-    return factory(this.getContainer(), {
-      props: {
-        appState: this.appState,
-      },
-    });
+  /**
+   * @stable [21.10.2018]
+   * @param {CreateElement} factory
+   * @returns {VueNodeT}
+   */
+  public render(factory: CreateElement): VueNodeT {
+    return factory(this.getContainer());
   }
 
+  // TODO
   private getContainer() {
     return this.routes[window.location.pathname] || this.routes['/not_found'];
   }
