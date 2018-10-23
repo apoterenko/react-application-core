b025e56de5404b2a4bd4f1ea2f84ccee09b2ad4d
alex.poter <a.poterenko@gmail.com>
2018-10-23 13:38:40 +0300

    route util fixes

--- src/core/util/route.ts
+++ src/core/util/route.ts
@@ -1,3 +1,6 @@
+import * as R from 'ramda';
+import * as URI from 'urijs';
+
 import { IEntity, IKeyValue, NEW_OPTION } from '../definitions.interface';
 
 export function buildRoute(path: string, params: IKeyValue): string {
@@ -18,3 +21,40 @@ export const buildEntityRoute = <TEntity extends IEntity>(path: string,
                                                           entity?: TEntity,
                                                           params?: IKeyValue): string =>
   buildRoute(path, {id: entity ? entity.id : NEW_OPTION, ...params});
+
+/**
+ * @stable [23.10.2018]
+ * @param {string} path
+ * @param {IKeyValue} routes
+ * @returns {{route: string; bindings: IKeyValue}}
+ */
+export const findUrlPattern = (path = '', routes: IKeyValue): { route: string, bindings: IKeyValue } => {
+  const bindings = {};
+  const currentUriPathParts = new URI(path).path().split('/');
+  let currentRoute = null;
+
+  R.forEachObjIndexed<IKeyValue>((value, routePattern) => {
+    const currentRouteUriParts = new URI(routePattern as string).path().split('/');
+
+    if (currentRouteUriParts.length === currentUriPathParts.length) {
+      currentRouteUriParts.forEach((currentRouteUriPart, index) => {
+        const currentUriPathPart = currentUriPathParts[index];
+
+        if (currentRouteUriPart.startsWith(':')) {
+          bindings[currentRouteUriPart] = currentUriPathPart;
+        } else if (currentRouteUriPart !== currentUriPathPart) {
+          currentRoute = false;
+        }
+      });
+
+      if (currentRoute !== false && R.isNil(currentRoute)) {
+        currentRoute = routePattern;
+      }
+    }
+  }, routes);
+
+  return {
+    route: currentRoute,
+    bindings,
+  };
+};
