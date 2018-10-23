5fe46f993c6b4311087d46d9f258e1fc588a25dd
alex.poter <a.poterenko@gmail.com>
2018-10-23 13:52:16 +0300

    vue-application.container fixes

--- src/core/component/application/vue-application.container.ts
+++ src/core/component/application/vue-application.container.ts
@@ -1,21 +1,43 @@
 import { CreateElement } from 'vue';
 import { Component } from 'vue-property-decorator';
+import { LoggerFactory } from 'ts-smart-logger';
 
+import { findUrlPattern, isFn } from '../../util';
 import { DI_TYPES, lazyInject } from '../../di';
+import { IKeyValue } from '../../definitions.interface';
 import { APPLICATION_SECTION } from '../application/application.interface';
 import { VueNodeT } from '../../vue-definitions.interface';
 import { VueBaseContainer } from '../base/vue-index';
 import { vueConnectorOptionsFactory } from '../connector/vue-index';
+import { ApplicationActionBuilder } from './application-action.builder';
 
 @Component(vueConnectorOptionsFactory({
   section$: APPLICATION_SECTION,
   customComputed$: [
     (state) => ({application: state.application})   // TODO move to common
   ],
+  forceUpdateOnChangeData$: (state) => state.application.path,
 }))
 export class VueApplicationContainer extends VueBaseContainer {
+  private static logger = LoggerFactory.makeLogger('VueApplicationContainer');
 
-  @lazyInject(DI_TYPES.Routes) private routes;
+  @lazyInject(DI_TYPES.Routes) private routes: IKeyValue;
+
+  /**
+   * @stable [23.10.2018]
+   */
+  constructor() {
+    super();
+
+    const prevOnPopState = window.onpopstate;
+    window.onpopstate = () => {
+      if (isFn(prevOnPopState)) {
+        prevOnPopState.call(window);
+      }
+      // Force call the render method when internal state updating. See forceUpdateOnChangeData$ above
+      this.dispatchCustomType(ApplicationActionBuilder.buildPathActionType(), location.pathname);
+    };
+  }
 
   /**
    * @stable [21.10.2018]
@@ -26,8 +48,24 @@ export class VueApplicationContainer extends VueBaseContainer {
     return factory(this.getContainer());
   }
 
-  // TODO
+  /**
+   * @stable [23.10.2018]
+   * @returns {string}
+   */
   private getContainer() {
-    return this.routes[window.location.pathname] || this.routes['/not_found'];
+    const pathname = location.pathname;
+    const routePatternResult = findUrlPattern(pathname, this.routes);
+
+    if (routePatternResult.route) {
+      VueApplicationContainer.logger.debug(
+        `[$VueApplicationContainer][getContainer] Router is recognized. Route result: ${
+          JSON.stringify(routePatternResult)}, pathname: ${pathname}`
+      );
+    } else {
+      VueApplicationContainer.logger.debug(
+        `[$VueApplicationContainer][getContainer] Router is not recognized. Route result: ${pathname}`
+      );
+    }
+    return this.routes[routePatternResult.route] || this.routes['/not_found'];
   }
 }
