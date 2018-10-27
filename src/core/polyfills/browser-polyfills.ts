3598eb3e8c6388bb5d399a2f7eb528da85ce45ef
alex.poter <a.poterenko@gmail.com>
2018-10-27 13:33:10 +0300

    smoothscroll-polyfill

--- src/core/polyfills/browser-polyfills.ts
+++ src/core/polyfills/browser-polyfills.ts
@@ -2,3 +2,5 @@ import './universal-polyfills';
 import 'classlist-polyfill';
 import 'matchmedia-polyfill/matchMedia';
 import 'matchmedia-polyfill/matchMedia.addListener.js';
+import * as smoothscroll from 'smoothscroll-polyfill';
+smoothscroll.polyfill();
