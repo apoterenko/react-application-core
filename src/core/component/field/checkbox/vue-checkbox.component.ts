311ad835120548592aaabaac4a53e058fb6754f5
alex.poter <a.poterenko@gmail.com>
2018-11-17 18:51:50 +0300

    vue-checkbox.component fixes

--- src/core/component/field/checkbox/vue-checkbox.component.ts
+++ src/core/component/field/checkbox/vue-checkbox.component.ts
@@ -1,13 +1,21 @@
 import { Component } from 'vue-property-decorator';
 
 import { VueNodeT, VueCreateElementFactoryT } from '../../../vue-definitions.interface';
+import { IVueEventsDefinitions } from '../../../vue-entities-definitions.interface';
 import { ComponentName } from '../../connector/vue-index';
-import { VueField, IVueFieldInputPropsEntity } from '../field/vue-index';
-import { AnyT } from '../../../definitions.interface';
+import { VueField } from '../field/vue-index';
 
 @ComponentName('vue-checkbox')
 @Component
 class VueCheckbox extends VueField {
+
+  /**
+   * @stable [17.11.2018]
+   */
+  constructor() {
+    super();
+    this.onInputClick = this.onInputClick.bind(this);
+  }
 
   /**
    * @stable [21.10.2018]
@@ -27,13 +35,11 @@ class VueCheckbox extends VueField {
   }
 
   /**
-   * @stable [13.11.2018]
-   * @returns {{[p: string]: () => AnyT}}
+   * @stable [17.11.2018]
+   * @returns {boolean}
    */
-  protected getMethods(): { [methodName: string]: () => AnyT } {
-    return {
-      ...super.getMethods(),
-    };
+  protected isInputWrapperFull(): boolean {
+    return false;
   }
 
   /**
@@ -44,13 +50,25 @@ class VueCheckbox extends VueField {
     return {
       ...super.getInputBindings(),
       type: 'checkbox',
+      checked: !!this.getValue(),
     };
   }
 
-  protected getInputListeners(): any {  // TODO
+  /**
+   * @stable [17.11.2018]
+   * @returns {IVueEventsDefinitions}
+   */
+  protected getInputListeners(): IVueEventsDefinitions {
     return {
       ...super.getInputListeners(),
-      click: () => this.onChange(!this.getValue()),
+      click: this.onInputClick,
     };
+  }
+
+  /**
+   * @stable [17.11.2018]
+   */
+  private onInputClick(): void {
+    this.onChange(!this.getValue());
   }
 }
