59259722d462f4ff9219aed02f2c163b5c4c5cc8
alex.poter <a.poterenko@gmail.com>
2018-11-20 02:04:36 +0300

    utils fixes

--- src/core/util/array.ts
+++ src/core/util/array.ts
@@ -1,6 +1,15 @@
+import * as R from 'ramda';
+
 /**
  * @stable [19.11.2018]
  * @param {number} length
  * @returns {any}
  */
 export const generateArray = (length: number) => Array.apply(null, {length});
+
+/**
+ * @stable [19.11.2018]
+ * @param {number[]} array
+ * @returns {number}
+ */
+export const arrayMinValue = (array: number[]): number => R.reduce(R.min, Infinity, array) as number;
