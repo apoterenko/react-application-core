3da78b973766819e685e0810c10a9546ae21fbc0
apoterenko <a.poterenko@gmail.com>
2019-02-05 22:22:09 +0300

    minor fixes

--- src/core/transport/response/transport-response.module.ts
+++ src/core/transport/response/transport-response.module.ts
@@ -1,5 +1,5 @@
 import { appContainer, DI_TYPES } from '../../di';
-import './accessor/transport-response-accessor.module';
 import { TransportResponseFactory } from './transport-response-factory.service';
+import './accessor/transport-response-accessor.module';
 
 appContainer.bind(DI_TYPES.TransportResponseFactory).to(TransportResponseFactory).inSingletonScope();
