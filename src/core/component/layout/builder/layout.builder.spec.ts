55129d35cfc17a98a29ad9a0c13ac6b287501713
alex.poter <a.poterenko@gmail.com>
2019-02-23 15:57:27 +0300

    build fixes

--- src/core/component/layout/builder/layout.builder.spec.ts
+++ src/core/component/layout/builder/layout.builder.spec.ts
@@ -5,29 +5,26 @@ import { UniversalLayoutBuilder } from './universal-layout.builder';
 describe('layout.builder', () => {
 
   it('test1', () => {
-    class PseudoElement {
-      private props;
-      constructor(name, value) {
-        this.props = {[name]: value};
-      }
+    function pseudoElement(name, value) {
+      this.props = {[name]: value};
     }
 
     const layoutBuilder = new UniversalLayoutBuilder(new DefaultLayoutViewBuilder());
     const layout = layoutBuilder.build({
       children: [
-        new PseudoElement('p1', 'v1'),
+        new pseudoElement('p1', 'v1'),
         {
           layout: LayoutBuilderTypeEnum.HORIZONTAL,
           children: [
-            new PseudoElement('p2', 'v2'),
+            new pseudoElement('p2', 'v2'),
             {
               factor: LayoutBuilderFactorEnum.FACTOR_2,
               children: [
-                new PseudoElement('p4', 'v4'),
-                new PseudoElement('p5', 'v5')
+                new pseudoElement('p4', 'v4'),
+                new pseudoElement('p5', 'v5')
               ],
             },
-            new PseudoElement('p3', 'v3')
+            new pseudoElement('p3', 'v3')
           ],
         }
       ],
@@ -35,20 +32,20 @@ describe('layout.builder', () => {
 
     expect(layout).toEqual({
       rows: [
-        new PseudoElement('p1', 'v1'),
+        new pseudoElement('p1', 'v1'),
         {
           columns: [
-            new PseudoElement('p2', 'v2'),
+            new pseudoElement('p2', 'v2'),
             {separator: null},
             {
               factor: LayoutBuilderFactorEnum.FACTOR_2,
               rows: [
-                new PseudoElement('p4', 'v4'),
-                new PseudoElement('p5', 'v5')
+                new pseudoElement('p4', 'v4'),
+                new pseudoElement('p5', 'v5')
               ],
             },
             {separator: null},
-            new PseudoElement('p3', 'v3')
+            new pseudoElement('p3', 'v3')
           ],
         }
       ],
