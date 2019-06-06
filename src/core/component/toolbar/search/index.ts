14a84bcbd7b9bf99800b31540e9e6eac5e258eea
alex.poter <a.poterenko@gmail.com>
2019-06-07 02:19:45 +0300

    react fixes

--- src/core/component/toolbar/search/index.ts
+++ src/core/component/toolbar/search/index.ts
@@ -1,3 +1,4 @@
 export * from './search-toolbar.component';
+export * from './search-toolbar.container';
 export * from './search-toolbar.interface';
 export * from './universal-search-toolbar.container';
