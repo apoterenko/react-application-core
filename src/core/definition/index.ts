b5052f3e9c383589ed7d7b25abfbc1bbef676177
alex.poter <a.poterenko@gmail.com>
2019-08-01 18:28:39 +0300

    minor fixes

--- src/core/definition/index.ts
+++ src/core/definition/index.ts
@@ -1,6 +1,5 @@
 export * from './address-definition.interface';
 export * from './api-definition.interface';
-export * from './blob-definition.interface';
 export * from './button-definition.interface';
 export * from './channel-definition.interface';
 export * from './entity-definition.interface';
