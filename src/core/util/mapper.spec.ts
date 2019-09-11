889fed089a7869d65c95411ece8955740a2279b2
alex.poter <a.poterenko@gmail.com>
2019-09-11 23:52:21 +0300

    add mapper.spec.ts

---
+++ src/core/util/mapper.spec.ts
@@ -0,0 +1,54 @@
+import { mapExtendedEntity } from './mapper';
+
+describe('util/mapper', () => {
+  describe('mapExtendedEntity', () => {
+    it('test1', () => {
+      const extendedEntity = mapExtendedEntity({name: 'name1', id: 100}, {changes: {name: 'name2'}});
+
+      expect(extendedEntity).toEqual({
+        entity: {name: 'name2', id: 100},
+        entityId: 100,
+        newEntity: false,
+        originalEntity: {name: 'name1', id: 100},
+      });
+    });
+
+    it('test2', () => {
+      const extendedEntity = mapExtendedEntity({name: 'name1'}, {changes: {name: 'name2'}});
+
+      expect(extendedEntity).toEqual({
+        entity: {name: 'name2'},
+        newEntity: true,
+        originalEntity: {name: 'name1'},
+      });
+    });
+
+    it('test3', () => {
+      const extendedEntity = mapExtendedEntity({name: 'name1'});
+
+      expect(extendedEntity).toEqual({
+        entity: {name: 'name1'},
+        newEntity: true,
+        originalEntity: {name: 'name1'},
+      });
+    });
+
+    it('test4', () => {
+      const extendedEntity = mapExtendedEntity(null);
+
+      expect(extendedEntity).toEqual({
+        entity: {},
+        newEntity: true,
+      });
+    });
+
+    it('test5', () => {
+      const extendedEntity = mapExtendedEntity(undefined);
+
+      expect(extendedEntity).toEqual({
+        entity: {},
+        newEntity: true,
+      });
+    });
+  });
+});
