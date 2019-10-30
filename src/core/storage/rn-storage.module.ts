ae9fb5af5d8e74d3fabe16f8a3ba801c2a17e60d
alex.poter <a.poterenko@gmail.com>
2019-10-30 10:54:54 +0300

    remove react native

--- src/core/storage/rn-storage.module.ts
+++
@@ -1,6 +0,0 @@
-import { appContainer, DI_TYPES } from '../di';
-import { RnStorage } from './rn-storage.service';
-import { STORAGE_NOT_VERSIONED_KEY, STORAGE_VERSIONED_KEY } from '../definition';
-
-appContainer.bind(DI_TYPES.Storage).toConstantValue(new RnStorage(STORAGE_VERSIONED_KEY));
-appContainer.bind(DI_TYPES.NotVersionedPersistentStorage).toConstantValue(new RnStorage(STORAGE_NOT_VERSIONED_KEY));
