11ee868d5d29d8155b1732b1685adb27fcc7d462
alex.poter <a.poterenko@gmail.com>
2019-11-17 11:13:02 +0300

    remove obsolete module

--- src/core/component/proxy/store/index.ts
+++ src/core/component/proxy/store/index.ts
@@ -1,5 +1,4 @@
 export * from './dialog-form-changes-confirm-store-proxy';
 export * from './dictionary-store-proxy';
 export * from './form-store-proxy';
-export * from './permission-store-proxy';
 export * from './transport-store-proxy';
--- src/core/component/proxy/store/permission-store-proxy/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './permission-store-proxy.interface';
-export * from './permission-store-proxy.proxy';
