6644d855adfdb66549317aa2ea028ea6dc7ea35c
alex.poter <a.poterenko@gmail.com>
2019-12-26 01:10:28 +0300

    add application/application-services-min.module

---
+++ src/core/application/application-services-min.module.ts
@@ -0,0 +1,11 @@
+import './universal.module';
+import '../bootstrap/bootstrap.module';
+import '../router/router.module';
+import '../storage/storage.module';
+import '../event/event-manager.module';
+import '../lock/lock.module';
+import '../api/entity/modify-entity-payload-factory.module';
+import '../component/dom-accessor/dom-accessor.module';
+import '../component/field/field.module';
+import '../component/ui/ui.module';
+import './application.effects';
--- src/core/channel/socket/socket-channel.module.ts
+++ src/core/channel/socket/socket-channel.module.ts
@@ -1,9 +1,11 @@
-import { appContainer, DI_TYPES } from '../../di';
+import {
+  bindInSingleton,
+  DI_TYPES,
+} from '../../di';
 
-import { IChannel } from '../channel.interface';
 import { SocketChannel } from './socket-channel.service';
 
 /**
  * @stable [11.12.2018]
  */
-appContainer.bind<IChannel>(DI_TYPES.Channel).to(SocketChannel).inSingletonScope();
+bindInSingleton(DI_TYPES.Channel, SocketChannel);
