8f05a72584bdab5166eda91c40ff3bb9b8572b97
alex.poter <a.poterenko@gmail.com>
2020-01-04 11:32:20 +0300

    add calendar.component

---
+++ src/core/component/calendar/calendar.component.tsx
@@ -0,0 +1,134 @@
+import * as React from 'react';
+
+import { BaseComponent } from '../base/base.component';
+import {
+  ICalendarDayEntity,
+  ICalendarEntity,
+  ICalendarProps,
+  ICalendarWeekEntity,
+  IGridColumnProps,
+  IListEntity,
+  INITIAL_LIST_ENTITY,
+  NOT_SELECTABLE_GRID_ENTITY,
+} from '../../definition';
+import { Grid } from '../grid';
+import {
+  isFn,
+  joinClassName,
+} from '../../util';
+
+export class Calendar extends BaseComponent<ICalendarProps> {
+
+  public static readonly defaultProps: ICalendarProps = {
+    showOnlyCurrentDays: false,
+  };
+
+  /**
+   * @stable [04.01.2020]
+   * @param {ICalendarProps} props
+   */
+  constructor(props: ICalendarProps) {
+    super(props);
+    this.onClick = this.onClick.bind(this);
+    this.getCellElement = this.getCellElement.bind(this);
+    this.getColumnClassName = this.getColumnClassName.bind(this);
+  }
+
+  /**
+   * @stable [04.01.2020]
+   * @returns {JSX.Element}
+   */
+  public render(): JSX.Element {
+    const props = this.props;
+    const calendar = this.calendarEntity;
+
+    const listEntity: IListEntity = {
+      ...INITIAL_LIST_ENTITY,
+      data: calendar.days,
+    };
+
+    const columns = calendar.daysLabels.map((day, index): IGridColumnProps => (
+      {
+        title: day,
+        index,
+        align: 'center',
+        columnClassName: this.getColumnClassName,
+        renderer: this.getCellElement,
+        onColumnClick: this.onClick,
+      }
+    ));
+
+    return (
+      <Grid
+        {...NOT_SELECTABLE_GRID_ENTITY}
+        highlightOdd={false}
+        headerRendered={false}
+        columnsConfiguration={columns}
+        className={joinClassName('rac-calendar', props.className)}
+        {...listEntity}
+      />
+    );
+  }
+
+  /**
+   * @stable [04.01.2020]
+   * @param {IGridColumnProps<ICalendarDayEntity>} payload
+   * @returns {string}
+   */
+  private getColumnClassName(payload: IGridColumnProps<ICalendarDayEntity>): string {
+    const {selectedDays = []} = this.props;
+    const entity = this.asCalendarDayEntity(payload);
+
+    return joinClassName(
+      selectedDays.includes(entity.value) && 'rac-calendar-selected-day'
+    );
+  }
+
+  /**
+   * @stable [04.01.2020]
+   * @param {ICalendarWeekEntity} item
+   * @param {IGridColumnProps<ICalendarDayEntity>} column
+   * @returns {JSX.Element}
+   */
+  private getCellElement(item: ICalendarWeekEntity, column: IGridColumnProps<ICalendarDayEntity>): JSX.Element {
+    const props = this.props;
+    const weekDayEntity: ICalendarDayEntity = item[column.index];
+
+    if (props.showOnlyCurrentDays && !weekDayEntity.current) {
+      return null;
+    }
+    if (isFn(props.renderer)) {
+      return props.renderer(weekDayEntity);
+    }
+    return <React.Fragment>{weekDayEntity.date.getDate()}</React.Fragment>;
+  }
+
+  /**
+   * @stable [04.01.2020]
+   * @param {IGridColumnProps} payload
+   */
+  private onClick(payload: IGridColumnProps): void {
+    const props = this.props;
+    if (isFn(props.onSelect)) {
+      props.onSelect(this.asCalendarDayEntity(payload));
+    }
+  }
+
+  /**
+   * @stable [04.01.2020]
+   * @param {IGridColumnProps} payload
+   * @returns {ICalendarDayEntity}
+   */
+  private asCalendarDayEntity(payload: IGridColumnProps): ICalendarDayEntity {
+    return payload.entity[payload.index];
+  }
+
+  /**
+   * @stable [04.01.2020]
+   * @returns {ICalendarEntity}
+   */
+  private get calendarEntity(): ICalendarEntity {
+    const props = this.props;
+    return props.calendarEntity || this.dc.asCalendar({useSyntheticCalendar: true});
+  }
+}
---
+++ src/core/definition/calendar-definition.interface.ts
@@ -0,0 +1,40 @@
+import {
+  ICalendarConfigurationWrapper,
+  IOnSelectWrapper,
+  IRendererWrapper,
+  ISelectedDaysWrapper,
+  IShowOnlyCurrentDaysWrapper,
+} from '../definitions.interface';
+import { IComponentProps } from './props-definition.interface';
+import {
+  ICalendarDayEntity,
+  ICalendarEntity,
+  ICalendarEntityWrapperEntity,
+} from './date-definition.interface';
+
+/**
+ * @stable [04.01.2020]
+ */
+export interface IGenericCalendarEntity
+  extends ICalendarConfigurationWrapper<ICalendarEntity>,
+    ICalendarEntityWrapperEntity,
+    ISelectedDaysWrapper<number[]>,
+    IShowOnlyCurrentDaysWrapper {
+}
+
+/**
+ * @stable [04.01.2020]
+ */
+export interface IBehavioralCalendarEntity
+  extends IGenericCalendarEntity,
+    IOnSelectWrapper<ICalendarDayEntity>,
+    IRendererWrapper<ICalendarDayEntity> {
+}
+
+/**
+ * @stable [03.01.2020]
+ */
+export interface ICalendarProps
+  extends IComponentProps,
+    IBehavioralCalendarEntity {
+}
