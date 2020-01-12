354472f76cdc1854d28842f0d0471ac2fb38015e
alex.poter <a.poterenko@gmail.com>
2020-01-12 19:04:08 +0300

    dict action builder

--- src/core/action/index.ts
+++ src/core/action/index.ts
@@ -1,4 +1,5 @@
 export * from './connector-action.builder';
 export * from './custom';
+export * from './dictionary-action.builder';
 export * from './router-action.builder';
 export * from './stack-action.builder';
