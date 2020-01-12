4e00bbef0849c1c2e0e8145514d72807235b1cdd
alex.poter <a.poterenko@gmail.com>
2020-01-12 19:26:01 +0300

    definitions fixes

--- src/core/component/google/maps/google-maps.interface.ts
+++ src/core/component/google/maps/google-maps.interface.ts
@@ -1,7 +1,6 @@
 import { IComponentProps } from '../../../definition';
 import {
   IItemWrapper,
-  IMarkerWrapper,
   IMenuOptionsWrapper,
   INameWrapper,
   IOnChangePlaceWrapper,
@@ -9,24 +8,12 @@ import {
   IOnInitWrapper,
   IOnSelectWrapper,
   IOptionsWrapper,
-  IPointsWrapper,
-  IRefreshMapWrapper,
-  IVisibleWrapper,
-  IZoomWrapper,
 } from '../../../definitions.interface';
 import {
-  IComponent,
+  IGoogleMapsMenuItemEntity,
   ILatLngEntity,
   IMenuItemEntity,
 } from '../../../definition';
-
-/**
- * @stable [31.07.2018]
- */
-export interface IGoogleMapsSelectEntity
-  extends IItemWrapper<IMenuItemEntity>,
-    ILatLngEntity {
-}
 
 /**
  * @stable [31.07.2018]
@@ -53,7 +40,7 @@ export enum GoogleMapsMapTypeEnum {
 export interface IGoogleMapsProps
   extends IComponentProps,
     IMenuOptionsWrapper<IMenuItemEntity[]>,
-    IOnSelectWrapper<IGoogleMapsSelectEntity>,
+    IOnSelectWrapper<IGoogleMapsMenuItemEntity>,
     IOptionsWrapper<google.maps.MapOptions>,
     IOnInitWrapper,
     IOnChangePlaceWrapper<IGoogleMapsMarkerChangePlaceEntity>,
@@ -66,33 +53,4 @@ export interface IGoogleMapsProps
 export interface IGoogleMapsClickPayloadEntity {
   pixel?: google.maps.Point;
   latLng?: google.maps.LatLng;
-}
-
-/**
- * @stable [23.02.2019]
- */
-export interface IGoogleMapsMarkerConfigEntity
-  extends ILatLngEntity,
-    IRefreshMapWrapper,
-    IVisibleWrapper,
-    IZoomWrapper,
-    IMarkerWrapper<string | google.maps.Marker> {
-}
-
-/**
- * @stable [05.03.2019]
- */
-export interface IGoogleMapsHeatMapLayerConfigEntity
-  extends IPointsWrapper<ILatLngEntity[]>,
-    IRefreshMapWrapper,
-    IZoomWrapper {
-}
-
-/**
- * @stable [31.07.2018]
- */
-export interface IGoogleMaps extends IComponent {
-  addMarker(cfg?: google.maps.MarkerOptions, name?: string): google.maps.Marker;
-  setMarkerState(cfg: IGoogleMapsMarkerConfigEntity): void;
-  addHeatMapLayer(cfg: IGoogleMapsHeatMapLayerConfigEntity): void;
 }
