1f495a630c238acd0c0e20326c94e486789056e4
alex.poter <a.poterenko@gmail.com>
2020-01-17 03:36:49 +0300

    add event-emitter-definition.interface

---
+++ src/core/definition/event-emitter-definition.interface.ts
@@ -0,0 +1,32 @@
+import {
+  AnyT,
+  IAutoUnsubscribingWrapper,
+  ICallbackWrapper,
+  IConditionWrapper,
+  IEventNameWrapper,
+} from '../definitions.interface';
+
+/**
+ * @stable [17.01.2020]
+ */
+export enum SyntheticEmitterEventsEnum {
+  SCROLL = 'scroll',
+}
+
+/**
+ * @stable [17.01.2020]
+ */
+export interface IEventEmitterSubscribeEntity
+  extends IAutoUnsubscribingWrapper,
+    ICallbackWrapper,
+    IConditionWrapper<(...args: AnyT[]) => void>,
+    IEventNameWrapper {
+}
+
+/**
+ * @stable [17.01.2020]
+ */
+export interface IEventEmitter {
+  emit(event: string, ...args: AnyT[]): void;
+  subscribe(cfg: IEventEmitterSubscribeEntity): () => void;
+}
