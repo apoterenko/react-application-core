ac4513e1b74de23966f579b2d295ea685c76b8d7
alex.poter <a.poterenko@gmail.com>
2020-05-09 16:59:22 +0300

    mappers fixes

--- src/core/util/mapper-component.ts
+++ src/core/util/mapper-component.ts
@@ -1,5 +1,9 @@
 import { GenericMappers } from './mapper-generic';
 import {
+  IFilterFormDialogContainerProps,
+  IFormContainerProps,
+  IFormProps,
+  IListContainerProps,
   IPageToolbarContainerProps,
   IPageToolbarProps,
   ISearchToolbarContainerProps,
@@ -7,61 +11,86 @@ import {
   IToolbarToolsContainerProps,
 } from '../definition';
 import { Selectors } from './select';
+import { IEntity } from '../definitions.interface';
 
 /**
  * @component-props-mapper
- * @stable [06.05.2020]
+ * @stable [09.05.2020]
  *
- * @param {IPageToolbarProps} props
- * @returns {IPageToolbarProps}
+ * @param {IFormProps<TEntity>} props
+ * @returns {IFormProps<TEntity>}
  */
+const mapFormProps = <TEntity = IEntity>(props: IFormProps<TEntity>): IFormProps<TEntity> =>
+  ({
+    ...GenericMappers.extendedEntity(props),
+    ...GenericMappers.formEntity(props),
+  });
+
 const mapPageToolbarProps = (props: IPageToolbarProps): IPageToolbarProps =>
   GenericMappers.paginatedLifeCycleEntity(props);
 
-/**
- * @component-props-mapper
- * @stable [06.05.2020]
- *
- * @param {ISearchToolbarProps} props
- * @returns {ISearchToolbarProps}
- */
 const mapSearchToolbarProps = (props: ISearchToolbarProps): ISearchToolbarProps =>
   GenericMappers.activeQueryEntity(props);
 
-/**
- * @container-props-mapper
- * @stable [06.05.2020]
- *
- * @param {IPageToolbarContainerProps} props
- * @returns {IPageToolbarContainerProps}
- */
 const mapPageToolbarContainerProps = (props: IPageToolbarContainerProps): IPageToolbarContainerProps =>
   ({
     ...GenericMappers.sectionNameWrapper(props),
-    ...GenericMappers.listWrapperEntity(props),
+    ...GenericMappers.listEntity(props),
   });
 
 /**
  * @container-props-mapper
- * @stable [06.05.2020]
+ * @stable [09.05.2020]
  *
- * @param {ISearchToolbarContainerProps} props
- * @returns {ISearchToolbarContainerProps}
+ * @param {IFormContainerProps} props
+ * @returns {IFormContainerProps}
  */
+const mapFormContainerProps = (props: IFormContainerProps): IFormContainerProps =>
+  ({
+    ...GenericMappers.extendedEntity(props),
+    ...GenericMappers.formEntity(props),
+    ...GenericMappers.sectionNameWrapper(props),
+  });
+
+/**
+ * @container-props-mapper
+ * @stable [09.05.2020]
+ *
+ * @param {IFilterFormDialogContainerProps} props
+ * @returns {IFilterFormDialogContainerProps}
+ */
+const mapFilterFormDialogContainerProps = (props: IFilterFormDialogContainerProps): IFilterFormDialogContainerProps =>
+  mapFormContainerProps(props);
+
+export const mapListContainerProps = (props: IListContainerProps): IListContainerProps =>
+  ({
+    ...GenericMappers.sectionNameWrapper(props),
+    ...GenericMappers.listEntity(props),
+  });
+
 const mapSearchToolbarContainerProps = (props: ISearchToolbarContainerProps): ISearchToolbarContainerProps =>
   ({
     ...GenericMappers.sectionNameWrapper(props),
     ...GenericMappers.queryFilterEntity(props),
-    ...GenericMappers.listWrapperEntity(props),
+    ...GenericMappers.listEntity(props),
   });
 
-/**
- * @container-component-props-mapper
- * @stable [07.05.2020]
- *
- * @param {IPageToolbarContainerProps} props
- * @returns {IPageToolbarProps}
- */
+const mapSearchToolbarContainerPropsAsSearchToolbarProps = (props: ISearchToolbarContainerProps): ISearchToolbarProps =>
+  ({
+    ...mapSearchToolbarProps(Selectors.queryFilter(props)),
+    ...GenericMappers.listEntityAsDisabled(props),
+    ...props.toolbarConfiguration,
+  });
+
+export const mapToolbarToolsContainerProps = (props: IToolbarToolsContainerProps): IToolbarToolsContainerProps =>
+  ({
+    ...GenericMappers.sectionNameWrapper(props),
+    toolbarTools: {
+      ...GenericMappers.listEntityAsDisabled(props),
+      activeTools: Selectors.activeToolbarToolsFromDirtyWrapper(Selectors.form(props)) || [],
+    },
+  });
+
 const mapPageToolbarContainerPropsAsPageToolbarProps = (props: IPageToolbarContainerProps): IPageToolbarProps =>
   ({
     ...mapPageToolbarProps(Selectors.list(props)),
@@ -70,35 +99,23 @@ const mapPageToolbarContainerPropsAsPageToolbarProps = (props: IPageToolbarConta
 
 /**
  * @container-component-props-mapper
- * @stable [06.05.2020]
- *
- * @param {IPageToolbarContainerProps} props
- * @returns {IPageToolbarProps}
+ * @stable [09.05.2020]
+ * @param {IFormContainerProps} props
+ * @returns {IFormProps}
  */
-const mapSearchToolbarContainerPropsAsSearchToolbarProps = (props: ISearchToolbarContainerProps): ISearchToolbarProps =>
+const mapFormContainerPropsAsFormProps = (props: IFormContainerProps): IFormProps =>
   ({
-    ...mapSearchToolbarProps(Selectors.queryFilter(props)),
-    ...GenericMappers.listWrapperEntityAsDisabledWrapper(props),
-    ...props.toolbarConfiguration,
-  });
-
-/**
- * @container-props-mapper
- * @stable [07.05.2020]
- */
-export const mapToolbarToolsContainerProps = (props: IToolbarToolsContainerProps): IToolbarToolsContainerProps =>
-  ({
-    ...GenericMappers.sectionNameWrapper(props),
-    toolbarTools: {
-      ...GenericMappers.listWrapperEntityAsDisabledWrapper(props),
-      activeTools: Selectors.dirtyWrapperActiveToolbarTools(Selectors.form(props)) || [],
-    },
+    ...mapFormProps(props),
+    ...props.formConfiguration,
   });
 
 /**
  * @stable [06.05.2020]
  */
 export class ComponentMappers {
+  public static filterFormDialogContainerProps = mapFilterFormDialogContainerProps;
+  public static formContainerProps = mapFormContainerProps;
+  public static formContainerPropsAsFormProps = mapFormContainerPropsAsFormProps;
   public static pageToolbarContainerProps = mapPageToolbarContainerProps;
   public static pageToolbarContainerPropsAsPageToolbarProps = mapPageToolbarContainerPropsAsPageToolbarProps;
   public static pageToolbarProps = mapPageToolbarProps;
