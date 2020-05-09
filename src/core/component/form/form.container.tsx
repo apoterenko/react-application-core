1374b428682a1265764f31d1703dcc48f0ceb8f6
alex.poter <a.poterenko@gmail.com>
2020-05-09 10:35:06 +0300

    form.container fixes

--- src/core/component/form/form.container.tsx
+++ src/core/component/form/form.container.tsx
@@ -1,10 +1,7 @@
 import * as React from 'react';
 import * as R from 'ramda';
 
-import {
-  AnyT,
-  IKeyValue,
-} from '../../definitions.interface';
+import { IKeyValue } from '../../definitions.interface';
 import { GenericContainer } from '../base';
 import { Form } from '../form';
 import {
@@ -14,42 +11,48 @@ import {
 } from '../../definition';
 import {
   ifNotNilThanValue,
-  mapFormProps,
+  Mappers,
 } from '../../util';
 
+/**
+ * @component-container-impl
+ * @stable [09.05.2020]
+ *
+ * Please use the "Mappers.formContainerProps"
+ */
 export class FormContainer extends GenericContainer<IFormContainerProps> {
 
   /**
-   * @stable [03.02.2020]
+   * @stable [09.05.2020]
    * @param {IFormContainerProps} props
    */
   constructor(props: IFormContainerProps) {
     super(props);
 
     this.onChange = this.onChange.bind(this);
-    this.onEmptyDictionary = this.onEmptyDictionary.bind(this);
-    this.onLoadDictionary = this.onLoadDictionary.bind(this);
+    this.onDictionaryEmpty = this.onDictionaryEmpty.bind(this);
+    this.onDictionaryLoad = this.onDictionaryLoad.bind(this);
     this.onReset = this.onReset.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
     this.onValid = this.onValid.bind(this);
   }
 
   /**
-   * @stable [18.04.2020]
+   * @stable [09.05.2020]
    * @returns {JSX.Element}
    */
   public render(): JSX.Element {
     const props = this.props;
+
     return (
       <Form
-        {...mapFormProps(props)}
-        {...this.formConfiguration}
+        {...Mappers.formContainerPropsAsFormProps(props)}
         onChange={this.onChange}
-        onSubmit={this.onSubmit}
+        onDictionaryEmpty={this.onDictionaryEmpty}
+        onDictionaryLoad={this.onDictionaryLoad}
         onReset={this.onReset}
+        onSubmit={this.onSubmit}
         onValid={this.onValid}
-        onEmptyDictionary={this.onEmptyDictionary}
-        onLoadDictionary={this.onLoadDictionary}
       >
         {props.children}
       </Form>
@@ -91,12 +94,22 @@ export class FormContainer extends GenericContainer<IFormContainerProps> {
     ifNotNilThanValue(this.formConfiguration.onSubmit, (onSubmit) => onSubmit(apiEntity));
   }
 
-  private onEmptyDictionary(dictionary: string, apiEntity: IApiEntity): void {
+  /**
+   * @stable [09.05.2020]
+   * @param {string} dictionary
+   * @param {IApiEntity} apiEntity
+   */
+  private onDictionaryEmpty(dictionary: string, apiEntity: IApiEntity): void {
     this.dictionaryStoreProxy.dispatchLoadDictionary(dictionary, apiEntity);
   }
 
-  private onLoadDictionary(items: AnyT): void {
-    const noAvailableItemsToSelect = this.settings.messages.noAvailableItemsToSelectMessage;
+  /**
+   * @stable [09.05.2020]
+   * @param {{}} items
+   */
+  private onDictionaryLoad(items: {}): void {
+    const noAvailableItemsToSelect = this.settings.messages.NO_AVAILABLE_ITEMS_TO_SELECT;
+
     if (noAvailableItemsToSelect && R.isEmpty(items)) {
       this.notificationStoreProxy.dispatchNotification(noAvailableItemsToSelect);
     }
