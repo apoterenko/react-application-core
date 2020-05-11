020295ffc9ffcd919de7938ee291782c0d4a021a
alex.poter <a.poterenko@gmail.com>
2020-05-11 12:02:31 +0300

    unsaved-form-changes-dialog.component fixes

--- src/core/component/dialog/unsaved-form-changes-dialog/unsaved-form-changes-dialog.component.ts
+++ src/core/component/dialog/unsaved-form-changes-dialog/unsaved-form-changes-dialog.component.ts
@@ -2,9 +2,15 @@ import {
   IActivateDialogConfigEntity,
   IUnsavedFormChangesDialogProps,
 } from '../../../definition';
-import { isFormChanged } from '../../../util';
+import { FormUtils } from '../../../util';
 import { BaseDialog } from '../base-dialog.component';
 
+/**
+ * @component-impl
+ * @stable [11.05.2020]
+ *
+ * Please use the "Mappers.TODO"
+ */
 export class UnsavedFormChangesDialog extends BaseDialog<IUnsavedFormChangesDialogProps> {
 
   public static readonly defaultProps: IUnsavedFormChangesDialogProps = {
@@ -12,11 +18,11 @@ export class UnsavedFormChangesDialog extends BaseDialog<IUnsavedFormChangesDial
   };
 
   /**
-   * @stable [06.05.2020]
+   * @stable [11.05.2020]
    * @param {IActivateDialogConfigEntity} payload
    */
   public activate(payload?: IActivateDialogConfigEntity): void {
-    if (isFormChanged(this.props)) { // We can't use dirty flag because of the default changes (!)
+    if (FormUtils.isChanged(this.props)) { // We can't use dirty flag because of the default changes (!)
       super.activate(payload);
     } else {
       this.onAcceptClick();
