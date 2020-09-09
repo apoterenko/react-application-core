8f760538d74f7ca4bfb46be573f14b0ad2a425e1
alex.poter <a.poterenko@gmail.com>
2020-09-09 23:56:21 +0300

    number converter fixes

--- src/core/converter/number/number-converter.interface.ts
+++ src/core/converter/number/number-converter.interface.ts
@@ -2,13 +2,13 @@ import { StringNumberT } from '../../definitions.interface';
 
 export interface INumberConverter {
   asNumber(value: StringNumberT): number;
-  currency(value: StringNumberT, options?: Intl.NumberFormatOptions): string;
-  format(value: StringNumberT): string;
-  fractionalFormat(value: StringNumberT): string;
+  format(value: StringNumberT, formatter?: Intl.NumberFormat, options?: Intl.NumberFormatOptions): string;
+  formatFractional(value: StringNumberT, options?: Intl.NumberFormatOptions): string;
+  formatFractionalCurrency(value: StringNumberT, options?: Intl.NumberFormatOptions): string;
+  formatInteger(value: StringNumberT, options?: Intl.NumberFormatOptions): string;
+  formatIntegerCurrency(value: StringNumberT, options?: Intl.NumberFormatOptions): string;
   id(value: StringNumberT): string;
   integer(value: StringNumberT, radix?: number): StringNumberT;
-  integerCurrency(value: StringNumberT): string;
-  integerFormat(value: StringNumberT): string;
   internalId(value: StringNumberT): string;
   number(value: StringNumberT, returnString?: boolean): StringNumberT;
   numberParameter(value: StringNumberT, converter?: (value: number) => number): number;
--- src/core/converter/number/number-converter.service.ts
+++ src/core/converter/number/number-converter.service.ts
@@ -20,40 +20,33 @@ export class NumberConverter implements INumberConverter {
   @lazyInject(DI_TYPES.Settings) private readonly settings: ISettingsEntity;
 
   private readonly defaultFormatter = new Intl.NumberFormat();
-  private readonly currencyFormatter: Intl.NumberFormat;
   private readonly integerCurrencyFormatter: Intl.NumberFormat;
-
-  /**
-   * @stable [22.10.2018]
-   */
-  private readonly defaultCurrencyFormatOptions: Intl.NumberFormatOptions;
-
-  private integerFormatter = new Intl.NumberFormat(this.locale, this.integerFormatOptions);
-  private fractionalFormatter = new Intl.NumberFormat(this.locale, this.fractionalFormatOptions);
-
-  /**
-   * @stable [22.10.2018]
-   */
-  private readonly defaultIntegerCurrencyFormatOptions: Intl.NumberFormatOptions;
+  private readonly fractionalCurrencyFormatter: Intl.NumberFormat;
+  private readonly integerFormatter = new Intl.NumberFormat(this.locale, this.integerFormatOptions);
+  private readonly fractionalFormatter = new Intl.NumberFormat(this.locale, this.fractionalFormatOptions);
 
   /**
    * @stable [22.10.2018]
    */
   constructor() {
-    this.currency = this.currency.bind(this);
     this.format = this.format.bind(this);
-    this.fractionalFormat = this.fractionalFormat.bind(this);
+    this.formatFractional = this.formatFractional.bind(this);
+    this.formatFractionalCurrency = this.formatFractionalCurrency.bind(this);
+    this.formatInteger = this.formatInteger.bind(this);
+    this.formatIntegerCurrency = this.formatIntegerCurrency.bind(this);
     this.id = this.id.bind(this);
-    this.integerCurrency = this.integerCurrency.bind(this);
-    this.integerFormat = this.integerFormat.bind(this);
 
-    this.defaultCurrencyFormatOptions = {style: 'currency', currency: this.settings.currency.currency};
-    this.currencyFormatter = new Intl.NumberFormat(this.locale, this.defaultCurrencyFormatOptions);
-    this.defaultIntegerCurrencyFormatOptions = {
-      ...this.defaultCurrencyFormatOptions,
+    /* @stable [09.09.2020] */
+    this.integerCurrencyFormatter = new Intl.NumberFormat(this.locale, {
+      ...this.currencyFormatOptions,
       ...this.integerFormatOptions,
-    };
-    this.integerCurrencyFormatter = new Intl.NumberFormat(this.locale, this.defaultIntegerCurrencyFormatOptions);
+    });
+
+    /* @stable [09.09.2020] */
+    this.fractionalCurrencyFormatter = new Intl.NumberFormat(this.locale, {
+      ...this.currencyFormatOptions,
+      ...this.fractionalFormatOptions,
+    });
   }
 
   /**
@@ -128,39 +121,50 @@ export class NumberConverter implements INumberConverter {
     return TypeUtils.isFn(converter) ? converter(v) : v;
   }
 
-  public format(value: string | number, formatter: {format(...args): string} = this.defaultFormatter): string {
-    return formatter.format(this.number(value));
-  }
-
-  public integerCurrency(value: StringNumberT): string {
-    return this.integerCurrencyFormatter.format(this.number(value) as number);
+  /**
+   * @stable [09.09.2020]
+   * @param value
+   * @param formatter
+   * @param options
+   */
+  public format(value: string | number, formatter = this.defaultFormatter, options?: Intl.NumberFormatOptions): string {
+    return this.doFormat(value, formatter, options);
   }
 
   /**
    * @stable [09.09.2020]
    * @param value
+   * @param options
    */
-  public integerFormat(value: StringNumberT): string {
-    return this.doFormat(value, this.integerFormatter);
+  public formatInteger(value: StringNumberT, options?: Intl.NumberFormatOptions): string {
+    return this.doFormat(value, this.integerFormatter, options);
   }
 
   /**
    * @stable [09.09.2020]
    * @param value
+   * @param options
    */
-  public fractionalFormat(value: StringNumberT): string {
-    return this.doFormat(value, this.fractionalFormatter);
+  public formatFractional(value: StringNumberT, options?: Intl.NumberFormatOptions): string {
+    return this.doFormat(value, this.fractionalFormatter, options);
   }
 
-  public currency(value: number | string, options?: Intl.NumberFormatOptions): string {
-    if (options) {
-      const currencyFormatter0 = new Intl.NumberFormat(
-          this.locale,
-          {...this.defaultCurrencyFormatOptions, ...options}
-      );
-      return this.format(value, currencyFormatter0);
-    }
-    return this.format(value, this.currencyFormatter);
+  /**
+   * @stable [09.09.2020]
+   * @param value
+   * @param options
+   */
+  public formatIntegerCurrency(value: StringNumberT, options?: Intl.NumberFormatOptions): string {
+    return this.doFormat(value, this.integerCurrencyFormatter, options);
+  }
+
+  /**
+   * @stable [09.09.2020]
+   * @param value
+   * @param options
+   */
+  public formatFractionalCurrency(value: StringNumberT, options?: Intl.NumberFormatOptions): string {
+    return this.doFormat(value, this.fractionalCurrencyFormatter, options);
   }
 
   /**
@@ -183,12 +187,24 @@ export class NumberConverter implements INumberConverter {
    * @stable [09.09.2020]
    * @param value
    * @param formatter
+   * @param options
    * @private
    */
-  private doFormat(value: StringNumberT, formatter: Intl.NumberFormat): string {
+  private doFormat(value: StringNumberT,
+                   formatter: Intl.NumberFormat,
+                   options?: Intl.NumberFormatOptions): string {
     return R.isNil(value) || !TypeUtils.isPositiveOrNegativeNumberLike(value)
       ? this.settings.numberConverter.na
-      : formatter.format(this.asNumber(value));
+      : (
+        R.isNil(options)
+          ? formatter.format(this.asNumber(value))
+          : (
+            new Intl.NumberFormat(this.locale, {
+              ...formatter.resolvedOptions(),
+              ...options,
+            }).format(this.asNumber(value))
+          )
+      );
   }
 
   /**
