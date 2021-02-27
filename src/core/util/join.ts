cbc22d94a151fe65235bfe49d978d575a8c170a3
alex poter <a.poterenko@gmail.com>
2021-02-27 19:50:36 +0300

    minor fixes

--- src/core/util/join.ts
+++ src/core/util/join.ts
@@ -1,6 +1,7 @@
 import { FilterUtils } from './filter';
 import { NvlUtils } from './nvl';
 import { ObjectUtils } from './object';
+import { TypeUtils } from './type';
 import { UniCodesEnum } from '../definitions.interface';
 
 /**
@@ -12,15 +13,16 @@ const join = (parts: unknown[], joiner: string = UniCodesEnum.SPACE): string =>
   FilterUtils.notNilValuesArrayFilter(...parts).join(joiner);
 
 /**
- * @stable [21.01.2021]
+ * @stable [25.02.2021]
  * @param parts
+ * @param valueHandler
  */
-const joinReduce = (parts: unknown[]): string =>
+const joinReduce = <TValue = unknown, TResult = TValue>(parts: TValue[], valueHandler?: ($value: TValue) => TResult): string =>
   ObjectUtils.isObjectNotEmpty(parts)
     ? (
       parts.length > 1
-        ? `${parts[0]}, ...`
-        : NvlUtils.nvl(parts[0], '')
+        ? `${TypeUtils.isFn(valueHandler) ? valueHandler(parts[0]) : parts[0]}, ...`
+        : NvlUtils.nvl(TypeUtils.isFn(valueHandler) ? valueHandler(parts[0]) : parts[0], '')
     )
     : '';
 
