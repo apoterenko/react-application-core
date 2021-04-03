763acb0888fe1057ba5ce1d1ae86a591ba3469da
alex poter <a.poterenko@gmail.com>
2021-04-03 10:48:21 +0300

    time utils fixes

--- src/core/util/time.ts
+++ src/core/util/time.ts
@@ -7,43 +7,48 @@ import {
 import { ConditionUtils } from './cond';
 
 /**
- * @stable [02.03.2019]
- * @param {number} hour
- * @param {string} pm
- * @param {string} am
- * @returns {string}
- */
-export const asUSATime = (hour: number,
-                          pm = ' PM',
-                          am = ' AM'): string =>
-  hour > 12 ? `${hour - 12}${pm}` : (hour === 12 ? `12${pm}` : `${hour}${am}`);
-
-/**
- * @stable [20.03.2021]
+ * @stable [03.04.2021]
  * @param time
  */
 const normalizeTime = (time: string): string => `${time.length > 1 ? time : ['0', time].join('')}`;
 
 /**
- * @stable [20.03.2021]
+ * @stable [03.04.2021]
  * @param time
  */
-const timeAtMinutesAsString = (time: number): string => {
+const fromSecondsToMinutes = (time: number): number => Math.floor(time / 60);
+
+/**
+ * @stable [03.04.2021]
+ * @param time
+ */
+const fromSecondsToMinutesAsString = (time: number): string => {
   if (R.isNil(time)) {
     return time;
   }
-  const minutes = String(Math.floor(time / 60));
+  const minutes = String(fromSecondsToMinutes(time));
   const seconds = String(time % 60);
 
   return `${normalizeTime(minutes)}:${normalizeTime(seconds)}`;
 };
 
 /**
- * @stable [20.03.2021]
+ * @stable [03.04.2021]
+ * @param hour
+ * @param pm
+ * @param am
+ */
+const fromHourToUsaHour = (hour: number,
+                           pm = ' PM',
+                           am = ' AM'): string =>
+  hour > 12 ? `${hour - 12}${pm}` : (hour === 12 ? `12${pm}` : `${hour}${am}`);
+
+/**
+ * @stable [03.04.2021]
  * @param time1
  * @param time2
  */
-const asDiffAtSeconds = (time1: string, time2: string): number => {
+const fromTimeToDiffAtSeconds = (time1: string, time2: string): number => {
   if (R.isNil(time1) || R.isNil(time2)) {
     return UNDEF;
   }
@@ -54,21 +59,24 @@ const asDiffAtSeconds = (time1: string, time2: string): number => {
 };
 
 /**
- * @stable [20.03.2021]
+ * @stable [03.04.2021]
  * @param time1
  * @param time2
  */
-const asDiffAtMinutes = (time1: string, time2: string = '00:00'): number =>
+const fromTimeToDiffAtMinutes = (time1: string, time2: string = '00:00'): number =>
   ConditionUtils.ifNotNilThanValue(
-    asDiffAtSeconds(time1, time2),
+    fromTimeToDiffAtSeconds(time1, time2),
     (diffAtSeconds) => diffAtSeconds / 60,
     UNDEF_SYMBOL
   );
 
 /**
- * @stable [20.03.2021]
+ * @utils
+ * @stable [03.04.2021]
  */
 export class TimeUtils {
-  public static readonly asDiffAtMinutes = asDiffAtMinutes;
-  public static readonly timeAtMinutesAsString = timeAtMinutesAsString;
+  public static readonly fromHourToUsaHour = fromHourToUsaHour;
+  public static readonly fromSecondsToMinutes = fromSecondsToMinutes;
+  public static readonly fromSecondsToMinutesAsString = fromSecondsToMinutesAsString;
+  public static readonly fromTimeToDiffAtMinutes = fromTimeToDiffAtMinutes;
 }
