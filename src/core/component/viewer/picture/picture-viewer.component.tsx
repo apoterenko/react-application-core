29d08340fe18c359ba6782b7dfb8c49abfa7b360
alex poter <a.poterenko@gmail.com>
2021-05-16 00:32:39 +0300

    picture-viewer.component fixes

--- src/core/component/viewer/picture/picture-viewer.component.tsx
+++ src/core/component/viewer/picture/picture-viewer.component.tsx
@@ -7,20 +7,41 @@ import {
 } from '../../../definition';
 import {
   ClsUtils,
+  ConditionUtils,
+  JoinUtils,
+  NvlUtils,
   PropsUtils,
 } from '../../../util';
 import { Viewer } from '../viewer.component';
 
+/**
+ * @component-impl
+ * @stable [16.05.2021]
+ */
 export class PictureViewer extends Viewer<IPictureViewerProps> {
 
+  /**
+   * @stable [15.05.2021]
+   */
   public static readonly defaultProps = PropsUtils.mergeWithParentDefaultProps<IPictureViewerProps>({
     defaultScr: DefaultEntities.MEDIA_NO_PICTURE_URL,
     usePreview: false,
   }, Viewer);
 
   /**
-   * @stable [13.12.2020]
-   * @protected
+   * @stable [15.05.2021]
+   */
+  public componentDidMount(): void {
+    super.componentDidMount();
+
+    // For only images because there no need to prefetch file manually
+    if (this.canPreview && this.originalProps.opened) {
+      this.showPreviewDialog();
+    }
+  }
+
+  /**
+   * @stable [15.05.2021]
    */
   protected getClassName(): string {
     return ClsUtils.joinClassName(
@@ -30,44 +51,65 @@ export class PictureViewer extends Viewer<IPictureViewerProps> {
   }
 
   /**
-   * @stable [13.12.2020]
-   * @protected
+   * @stable [15.05.2021]
    */
   protected get contentElement(): JSX.Element {
     return (
       <img
         className={ViewerClassesEnum.VIEWER_CONTENT}
         src={this.actualSrc}
-        style={this.domAccessor.getTransformScaleStyles(this.actualOrDefaultScale)}
-        alt=''/>
+        style={this.actualStyles}
+        alt=''
+        onClick={ConditionUtils.orUndef(this.canPreview, () => this.showPreviewDialog)}/>
     );
   }
 
   /**
-   * @stable [13.12.2020]
-   * @protected
+   * @stable [15.05.2021]
    */
   protected get previewElement(): JSX.Element {
     return (
       <PictureViewer
         src={this.actualSrc}
+        degree={this.actualOrDefaultPreviewDegree}
         scale={this.actualOrDefaultPreviewScale}/>
     );
   }
 
   /**
-   * @stable [13.12.2020]
-   * @protected
+   * @stable [15.05.2021]
    */
   protected get previewForwardActionElement(): JSX.Element {
     return null;
   }
 
   /**
-   * @stable [13.12.2020]
-   * @protected
+   * @stable [15.05.2021]
    */
   protected get previewBackActionElement(): JSX.Element {
     return null;
+  }
+
+  /**
+   * @stable [15.05.2021]
+   */
+  private get actualStyles(): React.CSSProperties {
+    const transformScaleStyles = this.domAccessor.getTransformScaleStyles(this.actualOrDefaultScale);
+    const transformRotateStyles = this.domAccessor.getTransformRotateStyles(this.actualOrDefaultDegree);
+
+    return ConditionUtils.ifNotNilThanValue(
+      NvlUtils.coalesce(
+        transformRotateStyles,
+        transformScaleStyles
+      ),
+      (styles): React.CSSProperties =>
+        this.domAccessor.getTransformStyles(
+          JoinUtils.join([
+            transformScaleStyles?.transform,
+            transformRotateStyles?.transform
+          ]),
+          styles.transformOrigin
+        )
+    );
   }
 }
