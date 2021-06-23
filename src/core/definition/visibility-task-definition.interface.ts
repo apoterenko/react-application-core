47049b111feab532a2679466191a72e12bd1f991
alex poter <a.poterenko@gmail.com>
2021-06-23 12:32:45 +0300

    scss fixes

---
+++ src/core/definition/visibility-task-definition.interface.ts
@@ -0,0 +1,8 @@
+import { IElementWrapper } from '../definitions.interface';
+
+/**
+ * @stable [16.06.2021]
+ */
+export interface IVisibilityTaskContextConfigEntity
+  extends IElementWrapper<HTMLElement> {
+}
